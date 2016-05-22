0bf0fbe9e7706bff5f6b729d457443dbc7212633
DayoAyeni <dedayoa@gmail.com>
2016-05-22 20:47:38 +0100

    Updated to reflect new models and changes

--- core/admin.py
+++ core/admin.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import CoAdmin, Contact, CoUser, EventType, Event, SentMessage, SMTPSetting, CoGroup
+from .models import CoAdmin, Contact, CoUser, MessageTemplate, \
+                    Event, PublicEvent, SentMessage, SMTPSetting, CoGroup
 
 class ContactAdmin(admin.ModelAdmin):
     pass
 
 
 admin.site.register(Event)
+admin.site.register(PublicEvent)
 admin.site.register(Contact)
 admin.site.register(SentMessage)
-admin.site.register(EventType)
+admin.site.register(MessageTemplate)
 admin.site.register(SMTPSetting)
 admin.site.register(CoAdmin)
 admin.site.register(CoUser)
