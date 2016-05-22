176c94f2365214a8234c992787fba6d38db6f41b
DayoAyeni <dedayoa@gmail.com>
2016-05-22 20:51:08 +0100

    Decided to use the pinax django-user-accounts.

--- keepintouch/urls.py
+++ keepintouch/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r"^account/", include('account.urls')),
     url(r'^', include('appsite.urls')),
 ]
