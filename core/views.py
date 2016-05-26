850bbdea357aa70eed149bc043f6c4fda62336a5
DayoAyeni <dedayoa@gmail.com>
2016-05-26 11:29:39 +0100

    Learnt to use Class based generic views and also struggled with
    getting POST data to save to the database. I guess what I missed was
    that I was already using modelforms

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse_lazy, reverse
 
 from django_tables2   import RequestConfig
 
 from .models import Contact, CoGroup, CoUser
+from .forms import ContactForm, NewContactForm
 from .tables import ContactTable
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
 # Create your views here.
 
@@ -30,7 +33,7 @@ def contacts(request):
         q_user = CoUser.objects.get(user=request.user)
         q_grps = q_user.group.all() #groups the user belongs to
         contactstable = ContactTable(Contact.objects.filter(created_by_group__in=q_grps))
-        RequestConfig(request).configure(contactstable)
+        RequestConfig(request, paginate={'per_page': 25}).configure(contactstable)
         params = {}
         params["title"] = "Contacts"
         params["table"] = contactstable
@@ -39,24 +42,72 @@ def contacts(request):
 
 class ContactView(View):
     
-    def get(self, request, cusid):
+    form_class = ContactForm
+    template_name = 'core/contacts/contact_detail.html'
+    params = {}
+    
+    def get(self, request, contactid):
+        q_user = get_object_or_404(Contact, pk=contactid)
+        self.params["form"] = self.form_class(instance=q_user)
+        self.params["title"] = "Contact {}".format(contactid)
+        return render(request,self.template_name, self.params)
+    
+    def post(self, request, contactid):
         
-        q_user = CoUser.objects.get(user=request.user)
-        q_grps = q_user.group_set.all()
+        q_user = get_object_or_404(Contact, pk=contactid)
+        form = self.form_class(request.POST or None, instance=q_user)
+        self.params["title"] = "Contact {}".format(contactid)
+        if form.is_valid():
+            #self.params["data"] = request.POST.get()
+            form.save()
+            #group = CoGroup.objects.get(pk=request.POST.get('created_by_group'))
+            #a = Contact(request.POST)
+            '''data = {
+                    #'slug' : contactid,
+                    'salutation':request.POST.get('salutation',''),
+                    'first_name':request.POST.get('first_name',''),
+                    'last_name':request.POST.get('last_name',''),
+                    'email':request.POST.get('email',''),
+                    'phone':request.POST.get('phone',''),
+                    'active':request.POST.get('active',''),
+                    'created_by_group': request.POST.get('created_by_group')
+                    }
+            c = Contact(data)
+            c.save()'''
+            #a.save()
+            return HttpResponseRedirect(reverse('core:contact-detail', args=[contactid]))
+        
+        return render(request, self.template_name, self.params)
 
-        contactstable = ContactTable(Contact.objects.filter(created_by_group=q_grps))
-        RequestConfig(request).configure(contactstable)
-        params = {}
-        params["cusid"] = cusid
-        params["title"] = 'Contact'
-        params["table"] = contactstable
-        params["grps"] = q_grps
-        return render(request, 'core/contacts/index.html', params)
+class ContactViewView(UpdateView):
     
-    def post(self, request):
-        return HttpResponse("Hallo World")
+    model = Contact
+    form_class = ContactForm
+    template_name = 'core/contacts/contact_detail.html'
     
-class NewContactView(View):
+    def get_context_data(self, **kwargs):
+        params = super(ContactViewView, self).get_context_data(**kwargs)
+        params["title"] = "Contact {}".format(self.object.pk)
+        return params
+
+class ContactDeleteView(DeleteView):
     
-    def get(self, request):
-        pass
+    model = Contact
+    
+    template_name = 'core/contacts/contact_detail.html'
+    success_url = reverse_lazy('core:contacts-list')
+    
+class ContactCreateView(CreateView):
+    
+    model = Contact
+    form_class = NewContactForm
+    template_name = 'core/contacts/contact_detail.html'
+    success_url = "/"
+    
+    def get_context_data(self, **kwargs):
+        params = super(ContactCreateView, self).get_context_data(**kwargs)
+        params["title"] = "New"
+        return params
+    
+    #fields = ['salutation','first_name','last_name','email','phone','active','created_by_group']
+
