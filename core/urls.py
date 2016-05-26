97efb4f43e1e566afd12a8662b386a1934a846dc
DayoAyeni <dedayoa@gmail.com>
2016-05-26 15:03:36 +0100

    url coding for contacts

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,10 @@ from .views import *
 urlpatterns = [
     url(r'^$', Index.as_view(), name='index'),
     url(r'^settings/$', settings, name='settings'),
-    url(r'^contacts/$', contacts, name='all_contact'), #lists all contacts
+    url(r'^contacts/$', contacts, name='contacts-list'), #lists all contacts
     url(r'^contact/', include([
-                url(r'^(?P<contactid>[A-Z0-9]{9})/$', ContactView.as_view(), name='contact_detail'),
-                url(r'^new/$', NewContactView.as_view(), name='new_contact')
+                url(r'^(?P<pk>[A-Z0-9]{9})/$', ContactViewView.as_view(), name='contact-detail'),
+                url(r'^(?P<pk>[A-Z0-9]{9})/delete/$', ContactDeleteView.as_view(), name='contact-delete'),
+                url(r'^new/$', ContactCreateView.as_view(), name='contact-new')
     ]))
 ]
