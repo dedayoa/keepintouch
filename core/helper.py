704dca6c3f44c1248f236ae050c471e76e0dacb0
DayoAyeni <dedayoa@gmail.com>
2016-05-27 15:40:28 +0100

    Crispy Form helper...but can become useful for other helpers

---
+++ core/helper.py
@@ -0,0 +1,14 @@
+'''
+Created on May 27, 2016
+
+@author: Dayo
+'''
+
+from crispy_forms.helper import FormHelper
+
+class EventFormSetHelper(FormHelper):
+    
+    def __init__(self, *args, **kwargs):
+        super(EventFormSetHelper, self).__init__(*args, **kwargs)
+        self.form_method = 'post'
+        
