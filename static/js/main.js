fe4b0539b5bce80ea357864a8ff0f07c4e21573c
DayoAyeni <dedayoa@gmail.com>
2016-05-27 15:21:22 +0100

    Implemented datepicker
    THis involves the javascript and including the necessary
    foundation-datepicker library

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,10 @@
  * 
  */
 
+$( document ).ready(function() {
+	$('.event-form-date').fdatepicker({
+		/*initialDate: '02-12-1989',*/
+		format: 'dd-mm-yyyy',
+		disableDblClickSelection: true
+	});
+});
