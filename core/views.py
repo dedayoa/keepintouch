6e988173d84439fdf594705f7e902a80787c0284
DayoAyeni <dedayoa@gmail.com>
2016-05-27 15:30:40 +0100

    Work on views, especially the generic class view for contact to support
    events formset

--- core/views.py
+++ core/views.py
@@ -5,8 +5,8 @@ from django.core.urlresolvers import reverse_lazy, reverse
 
 from django_tables2   import RequestConfig
 
-from .models import Contact, CoGroup, CoUser
-from .forms import ContactForm, NewContactForm
+from .models import Contact, CoGroup, CoUser, Event
+from .forms import ContactForm, NewContactForm, EventFormSet, EventFormSetHelper
 from .tables import ContactTable
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 
@@ -43,6 +43,7 @@ def contacts(request):
 class ContactView(View):
     
     form_class = ContactForm
+    event_formset = EventFormSet()
     template_name = 'core/contacts/contact_detail.html'
     params = {}
     
@@ -50,6 +51,10 @@ class ContactView(View):
         q_user = get_object_or_404(Contact, pk=contactid)
         self.params["form"] = self.form_class(instance=q_user)
         self.params["title"] = "Contact {}".format(contactid)
+        self.params["contactid"] = contactid
+        
+        self.params["event_formset"] = EventFormSet(instance=q_user)
+        
         return render(request,self.template_name, self.params)
     
     def post(self, request, contactid):
@@ -85,12 +90,77 @@ class ContactViewView(UpdateView):
     model = Contact
     form_class = ContactForm
     template_name = 'core/contacts/contact_detail.html'
+    event_formset = EventFormSet()
+    params = {}
+    #event_formset_helper = EventFormSetHelper()
+    
+    def get(self, request, *args, **kwargs):
+        super(ContactViewView, self).get(request, *args, **kwargs)
+        #q1 = Contact.objects.filter(pk=self.object.pk)
+        
+        #q1 = get_object_or_404(Contact, pk=self.object.pk)
+        #queryset = Event.objects.filter(contact=q1)
+        #q2 = q1.event_set.all()
+        #self.object = self.get_object() #can pass queryset
+        #form_class = self.get_form_class()
+        #form = self.get_form(form_class)
+        event_line_item_form = EventFormSet(instance=self.object)
+        
+        
+        self.params["form"] = self.form_class(instance=self.object)
+        
+        self.params["event_formset"] = event_line_item_form
+        
+        return render(request,self.template_name, self.params)
+        '''
+        return self.render_to_response(self.get_context_data(
+                form = form,
+                event_line_item_form = event_line_item_form
+                )
+            )
+        '''
+        
+        
+    def post(self, request, *args, **kwargs):
+        #super(ContactViewView, self).post(request, *args, **kwargs)
+        self.object = self.get_object()
+        #form_class = self.get_form_class()
+        form = self.get_form(self.form_class)
+        
+        #form = self.form_class(instance=self.object)
+        event_formset = EventFormSet(request.POST, instance=self.object)
+        
+        
+        if form.is_valid() and event_formset.is_valid():
+            return self.form_valid(form, event_formset)
+        return self.form_invalid(form, event_formset)
+        
+        #return render(request,self.template_name, self.params)
+    
+    def form_valid(self, form, event_line_item_form):
+        self.object = form.save()
+        #event_line_item_form.instance = self.object
+        event_line_item_form.save()
+        return HttpResponseRedirect(reverse('core:contact-detail', args=[self.object.pk]))
+        #return HttpResponseRedirect(self.object.get_absolute_url())
+    
+    def form_invalid(self, form, event_line_item_form):
+        
+        return HttpResponseRedirect(reverse('core:contact-detail', args=[self.object.pk]))
+        '''
+        return self.render_to_response(
+            self.get_context_data(
+                form = form,
+                event_line_item_form = event_line_item_form
+            )
+        )'''
     
     def get_context_data(self, **kwargs):
-        params = super(ContactViewView, self).get_context_data(**kwargs)
-        params["title"] = "Contact {}".format(self.object.pk)
-        params["contactid"] = self.object.pk
-        return params
+        self.params = super(ContactViewView, self).get_context_data(**kwargs)
+        self.params["title"] = "Contact {}".format(self.object.pk)
+        self.params["contactid"] = self.object.pk
+
+        return self.params
 
 class ContactDeleteView(DeleteView):
     
@@ -119,3 +189,6 @@ class ContactCreateView(CreateView):
     
     #fields = ['salutation','first_name','last_name','email','phone','active','created_by_group']
 
+class EventCreateView(CreateView):
+    
+    model = Event
