16e45a4c24ab25dc2eb8aef47880a20fd4c97fe2
DayoAyeni <dedayoa@gmail.com>
2016-06-01 00:00:54 +0100

    Got tired of Co in model class names. 3 days work on M2M

--- core/models.py
+++ core/models.py
@@ -1,4 +1,7 @@
-from django.db import models
+import sys
+
+from django.db import models, transaction
+
 from phonenumber_field.modelfields import PhoneNumberField
 from django.contrib.auth.models import User
 from fernet_fields import EncryptedCharField
@@ -8,6 +11,10 @@ from randomslugfield import RandomSlugField
 from django.contrib.postgres.fields import JSONField
 from django.core.urlresolvers import reverse
 from django.utils import timezone
+from django.db.models.signals import post_save, pre_save, m2m_changed
+from django.core.exceptions import ValidationError
+from django.http import request
+from django.dispatch.dispatcher import receiver
 
 ### Managers
 class ActiveManager(models.Manager):
@@ -16,16 +23,46 @@ class ActiveManager(models.Manager):
         return super(ActiveManager, self).get_queryset().filter(active=True)
 
     
-class CoGroup(models.Model):
-    title = models.CharField(max_length=100, blank=False)
-    description = models.CharField(max_length=255, blank=True)
-    active = models.BooleanField()
+class KITUserManager(models.Manager):
     
-    def __str__(self):
-        return self.title
-    
+    def get_contacts(self):
+        return    
 
-class CoUser(models.Model):
+def get_default_user_group():
+    
+    data = {
+        'title':'Default',
+        'description':'Default User Group',
+        'kit_admin': 'self',
+        'active': True,
+    }
+            
+    return CoUserGroup.objects.get_or_create(**data)
+
+def create_and_set_default_user_group(sender, instance, *args, **kwargs):
+    
+    if instance.is_admin:
+        
+        #if not CoUserGroup.objects.filter(title='Default', kit_admin=instance).exists():
+        
+        kitadmin = KITUser.objects.get(pk=instance.id)
+        
+        try:
+            coug, created = CoUserGroup.objects.get_or_create(
+                    title = 'Default',
+                    description = 'Default User Group',
+                    kit_admin = kitadmin,
+                    defaults = {
+                        'active' : True
+                                }
+                )
+            if created:
+                instance.user_group = coug
+        except:
+            print("Error:", sys.exc_info())
+
+
+class KITUser(models.Model):
     
     INDUSTRY = (
         ('AVIATION','Aviation'),
@@ -33,29 +70,77 @@ class CoUser(models.Model):
         ('RELIGIOUS','Religious'),
         ('FINBANK','Finance & Banking'),
         ('OTHER','Other')
-                )
+    )
     
     user = models.OneToOneField(User)
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    dob = models.DateField(blank=False)
     
-    phone_number = PhoneNumberField(blank=True, unique=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    phone_number = PhoneNumberField(blank=True, null=True, unique=True)
     company = models.CharField(max_length=255, blank=True)
     industry = models.CharField(max_length=50, choices=INDUSTRY, blank=False)
     address_1 = models.CharField(max_length=100, blank=False)
     address_2 = models.CharField(max_length=100, blank=True)
     city_town = models.CharField(max_length=100, blank=False)
     state = models.CharField(max_length=100, blank=False)
-    dob = models.DateField(blank=False)
-    group = models.ManyToManyField(CoGroup)
-    is_coadmin = models.BooleanField(default=False)
-
+    cou_group = models.ForeignKey('CoUserGroup', models.PROTECT, null=True, blank=True)
+    is_admin = models.BooleanField(default=False)
+    
+    objects = KITUserManager()
     
     def __str__(self):
         if self.user.first_name: return self.user.first_name
         else: return self.user.username
+        
+    def get_parent(self):
+        if self.is_admin:
+            return False
+        return self.parent
+    
+    def get_contacts(self):
+        if self.is_admin:
+            #return self.kituser_set.contact_set
+            return Contact.objects.filter(kit_user__parent=self.pk)
+        else:
+            return self.contact_set.all()
 
-class CoAccount(models.Model):
-    user = models.OneToOneField(CoUser)
+post_save.connect(create_and_set_default_user_group, sender=KITUser)      
+
+
+def prevent_save_of_group_titled_default(sender, instance, *args, **kwargs):
+    
+    if (instance.title).lower() == 'default' :
+        pass
+
+          
+class CoUserGroup(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=255, blank=True)
+    kit_admin = models.ForeignKey(KITUser, on_delete=models.CASCADE, blank=False)
+    active = models.BooleanField() #when deactivated, 
+    
+    def __str__(self):
+        return self.title
+    
+    def get_contact_managed_by_group(self):
+        if self.active:
+            return self.contact
+    '''    
+    def save(self, *args, **kwargs):
+        #prevent save if title is default or Default
+        if (self.title).lower() != 'default':
+            super(CoUserGroup, self).save(*args, **kwargs)
+        elif request.user.is_admin:            
+            raise ValidationError('You cannot save a Group as "default"')
+    '''    
+    class Meta:
+        verbose_name = 'User Group'
+        
+
+        
+
+class KITAdminAccount(models.Model):
+    kit_admin = models.OneToOneField(KITUser)
     sms_balance = models.DecimalField(max_digits=12, decimal_places=4)
     last_subscribed = models.DateTimeField()
     subscription_expires = models.DateField()
@@ -77,13 +162,22 @@ class SMTPSetting(models.Model):
     smtp_password = EncryptedCharField(max_length=255,blank=True)
     active = models.BooleanField()
     
-    created_by_group = models.ForeignKey(CoGroup,models.SET_NULL, null=True)
+    kit_admin = models.ForeignKey(KITUser, models.CASCADE, blank=False)
+    
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return "%s - %s"%(self.description,self.smtp_server)
 
+
+def set_m2m_contact_usergroup(sender, instance, created, **kwargs):
+    
+    if created:
+        transaction.on_commit(
+             lambda: instance.cou_group.add(instance.kit_user.cou_group)
+        )
+        
 
 class Contact(models.Model):
     
@@ -104,7 +198,9 @@ class Contact(models.Model):
     #slug = models.SlugField(max_length=100)
     active = models.BooleanField(default=True)
     
-    created_by_group = models.ForeignKey(CoGroup,models.SET_NULL, null=True, blank=True)
+    kit_user = models.ForeignKey(KITUser, models.PROTECT)
+    cou_group = models.ManyToManyField('CoUserGroup', blank=True)
+    
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
 
@@ -112,13 +208,28 @@ class Contact(models.Model):
         return self.first_name
     
     @property
-    def group(self):
-        return '{}'.format(self.created_by_group)
+    def get_usergroup_contact_managed_by(self):
+        return '{}'.format(self.kit_user.cou_group)
     
     def get_absolute_url(self):
         return reverse('core:contact-detail',
                        args=[self.slug])
         
+    def all_cou_group(self):
+        return ', '.join([x.title for x in self.cou_group.all()])
+
+post_save.connect(set_m2m_contact_usergroup, sender=Contact) 
+
+    
+class ContactGroup(models.Model):
+    title = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=255, blank=True)
+    contacts = models.ManyToManyField(Contact, blank=False)
+    kit_user = models.ForeignKey(KITUser, models.PROTECT, blank=False)
+    active = models.BooleanField()
+    
+    def __str__(self):
+        return self.title    
 
 class MessageTemplate(models.Model):
     title = models.CharField(max_length=100)
@@ -127,12 +238,21 @@ class MessageTemplate(models.Model):
     smtp_setting = models.ForeignKey(SMTPSetting)
     send_sms = models.BooleanField()
     
-    created_by_group = models.ForeignKey(CoGroup,models.SET_NULL, null=True)
+    #user_group = models.ForeignKey(CoUserGroup,models.SET_NULL, null=True)
+    kit_admin = models.ForeignKey(KITUser, models.PROTECT, blank=True)
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
+    
+    @property
+    def get_usergroup_template_belongs_by(self):
+        return '{}'.format(self.kit_admin.user_group)
+
+
+class EventManager(models.Manager):
+    pass
        
 class Event(models.Model):
     '''
@@ -144,6 +264,7 @@ class Event(models.Model):
     title = models.CharField(max_length=100, blank=False)
     
     #created_by_group = models.ForeignKey(CoGroup,models.SET_NULL, null=True)
+    #couser = models.ForeignKey(CoUser, models.SET_NULL, blank=True)
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
     
@@ -172,7 +293,8 @@ class PublicEvent(models.Model):
     #applies_to = models.CharField(max_length=3, choices=APPLIESTO, default='ALL')
     recipients = models.ManyToManyField(Contact, blank=True)
     
-    #created_by_group = models.ForeignKey(CoGroup,models.SET_NULL, null=True)
+    #event_group = models.ForeignKey(CoUserGroup,models.SET_NULL, null=True)
+    kit_user = models.ForeignKey(KITUser, models.PROTECT, blank=True)
     created = models.DateTimeField(auto_now=True)
     last_modified = models.DateTimeField(auto_now_add=True)
     
@@ -180,8 +302,14 @@ class PublicEvent(models.Model):
         return "%s"%(self.title)
     
     @property
-    def group(self):
-        return '{}'.format(self.created_by_group)
+    def get_group(self):
+        return '{}'.format(self.kit_user.user_group)
+    
+    def get_absolute_url(self):
+        return reverse('core:public-event-detail',
+                       args=[self.pk])
+
+
     
 class SentMessage(models.Model):
     event = models.ForeignKey(Event)
