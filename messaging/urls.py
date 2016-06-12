8d230115d46e24635cb83cf0bceb4174358d462c
DayoAyeni <dedayoa@gmail.com>
2016-06-12 18:07:06 +0100

    URLconf for deleting

--- messaging/urls.py
+++ messaging/urls.py
@@ -16,8 +16,14 @@ urlpatterns = [
     url(r'^standard/', include([
                 url(r'^new/$', StandardMessageCreateView.as_view(), name='new-standard-message'),
                 url(r'^draft/(?P<pk>\d+)/$', StandardMessageUpdateDraftView.as_view(), name='standard-message-draft'),
+                url(r'^message/(?P<pk>\d+)/delete/$', StandardMessageDeleteView.as_view(), name='standard-message-delete'),
                 url(r'^(draft|waiting|processed)/$', StandardMessageCreateView.as_view(), name='message-list-by-status'),
                 ])),
-    url(r'^advanced/new/$', AdvancedMessageCreateView.as_view(), name='new-advanced-message'),
+    url(r'^advanced/', include([
+                url(r'^new/$', AdvancedMessageCreateView.as_view(), name='new-advanced-message'),
+                url(r'^draft/(?P<pk>\d+)/$', AdvancedMessageUpdateDraftView.as_view(), name='advanced-message-draft'),
+                url(r'^message/(?P<pk>\d+)/delete/$', AdvancedMessageDeleteView.as_view(), name='advanced-message-delete'),
+                url(r'^(draft|waiting|processed)/$', StandardMessageCreateView.as_view(), name='message-list-by-status'),
+                ])),
     
 ]
