7f25b844ad4b309592a7800bf36b41425c4120a4
DayoAyeni <dedayoa@gmail.com>
2016-06-12 18:06:49 +0100

    Views for advancedmessaging as well as StandardMessagingDeleteView

--- messaging/views.py
+++ messaging/views.py
@@ -3,12 +3,12 @@ from django.utils.translation import ugettext_lazy as _, ugettext
 
 from django_tables2   import RequestConfig
 
-from .models import StandardMessaging
+from .models import StandardMessaging, AdvancedMessaging
 from core.models import KITUser 
-from .forms import StandardMessagingForm
+from .forms import StandardMessagingForm, AdvancedMessagingForm
 from .tables import *
 from django.http.response import HttpResponse
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy, reverse
 
 # Create your views here.
@@ -27,7 +27,7 @@ def messaging_standard(request):
         #params["table"] = s_message_table
         return render(request, 'core/events/public_events_index.html', params)
 
-def messaging_advanced(request):
+def messaging_by_status(request):
     
     return HttpResponse()
 
@@ -64,6 +64,7 @@ class StandardMessageUpdateDraftView(UpdateView):
         params = super(StandardMessageUpdateDraftView, self).get_context_data(**kwargs)
         params["title"] = _("Message")
         params["draft_time"] = self.object.last_modified
+        params["messageid"] = self.object.pk  
         
         return params
         
@@ -71,12 +72,55 @@ class StandardMessageUpdateDraftView(UpdateView):
     
 class AdvancedMessageCreateView(CreateView):
     
-    model = StandardMessaging
-    form_class = StandardMessagingForm
-    template_name = 'messaging/standard/new_standard_message.html'
+    model = AdvancedMessaging
+    form_class = AdvancedMessagingForm
+    template_name = 'messaging/advanced/new_advanced_message.html'
     
     def form_valid(self, form):
         form.instance.created_by = self.request.user.kituser
 
-        return super(StandardMessageCreateView, self).form_valid(form)
+        return super(AdvancedMessageCreateView, self).form_valid(form)
     
+    def get_context_data(self, **kwargs):
+        self.params = super(AdvancedMessageCreateView, self).get_context_data(**kwargs)
+        self.params["title"] = _("Create Advanced Message")        
+        return self.params
+    
+    def get_success_url(self):
+        return reverse('messaging:advanced-message-draft', kwargs={'pk': self.object.pk})
+    
+
+class AdvancedMessageUpdateDraftView(UpdateView):
+    
+    model = AdvancedMessaging
+    form_class = AdvancedMessagingForm
+    template_name = 'messaging/advanced/advanced_message_draft.html'
+    
+    
+    def get_context_data(self, **kwargs):
+        params = super(AdvancedMessageUpdateDraftView, self).get_context_data(**kwargs)
+        params["title"] = _("Advanced Message")
+        params["draft_time"] = self.object.last_modified
+        params["messageid"] = self.object.pk       
+        return params
+    
+class StandardMessageDeleteView(DeleteView):
+    
+    model = StandardMessaging
+    success_url = reverse_lazy('messaging:new-standard-message')
+    
+    def get_context_data(self, **kwargs):
+        params = super(StandardMessageDeleteView, self).get_context_data(**kwargs)
+        params["title"] = "Deleting Message"
+        return params
+    
+
+class AdvancedMessageDeleteView(DeleteView):
+    
+    model = AdvancedMessaging
+    success_url = reverse_lazy('messaging:new-advanced-message')
+    
+    def get_context_data(self, **kwargs):
+        params = super(AdvancedMessageDeleteView, self).get_context_data(**kwargs)
+        params["title"] = "Deleting Message"
+        return params
