c67281a4112cc0e578f613b9ee56b6e3b532c531
DayoAyeni <dedayoa@gmail.com>
2016-06-13 01:58:51 +0100

    Worked on the URLconf for the message_processed_status view

--- messaging/urls.py
+++ messaging/urls.py
@@ -12,17 +12,17 @@ from .views import *
 
 urlpatterns = [
     url(r'^create/success/$', StandardMessageCreateView.as_view(), name=''),
-    #url(r'^messages/(?P<msgstat>draft|processed)/$', message_status_view, name='message-list-by-status'),
+    url(r'^processed/messages/$', message_processed_status_view, name='messages-processed-status'),
     url(r'^standard/', include([
                 url(r'^new/$', StandardMessageCreateView.as_view(), name='new-standard-message'),
                 url(r'^draft/(?P<pk>\d+)/$', StandardMessageUpdateDraftView.as_view(), name='standard-message-draft'),
-                url(r'^(?P<msgstat>draft|processed)/messages/$', standard_message_status_view, name='standard-messages-list'),
+                url(r'^draft/messages/$', standard_message_draft_view, name='standard-draft-messages'),
                 url(r'^message/(?P<pk>\d+)/delete/$', StandardMessageDeleteView.as_view(), name='standard-message-delete'),
                 ])),
     url(r'^advanced/', include([
                 url(r'^new/$', AdvancedMessageCreateView.as_view(), name='new-advanced-message'),
                 url(r'^draft/(?P<pk>\d+)/$', AdvancedMessageUpdateDraftView.as_view(), name='advanced-message-draft'),
-                url(r'^(?P<msgstat>draft|processed)/messages/$', advanced_message_status_view, name='advanced-messages-list'),
+                url(r'^draft/messages/$', advanced_message_draft_view, name='advanced-draft-messages'),
                 url(r'^message/(?P<pk>\d+)/delete/$', AdvancedMessageDeleteView.as_view(), name='advanced-message-delete'),
                 
                 ])),
