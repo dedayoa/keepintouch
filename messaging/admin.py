a7360d073324063ca9a6f79a94950d5a7e7c462d
DayoAyeni <dedayoa@gmail.com>
2016-06-13 01:54:52 +0100

    registered processed messages

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import StandardMessaging, AdvancedMessaging
+from .models import StandardMessaging, AdvancedMessaging, ProcessedMessages
 
 admin.site.register(StandardMessaging)
 admin.site.register(AdvancedMessaging)
+admin.site.register(ProcessedMessages)
