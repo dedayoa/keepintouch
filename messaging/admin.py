94e258cb78fd96df9f123fe168caf579d2a540ce
DayoAyeni <dedayoa@gmail.com>
2016-06-15 20:55:38 +0100

    Implemented Queued messages in admin

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import StandardMessaging, AdvancedMessaging, ProcessedMessages
+from .models import StandardMessaging, AdvancedMessaging, ProcessedMessages,\
+                    QueuedMessages
 
 admin.site.register(StandardMessaging)
 admin.site.register(AdvancedMessaging)
 admin.site.register(ProcessedMessages)
+admin.site.register(QueuedMessages)
