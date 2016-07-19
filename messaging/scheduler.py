2980b8b7cf1b2bf8f2301cfe63a83af67114b686
DayoAyeni <dedayoa@gmail.com>
2016-07-19 23:43:39 +0100

    Implemented scheduler for one-time-event

--- messaging/scheduler.py
+++ messaging/scheduler.py
@@ -9,7 +9,7 @@ import django_rq
 from rq_scheduler import Scheduler
 
 from datetime import datetime
-from .tasks import process_private_anniversary, process_public_anniversary
+from .tasks import process_private_anniversary, process_public_anniversary, process_onetime_event
 
 default_scheduler = django_rq.get_scheduler('default')
 
@@ -21,7 +21,7 @@ default_scheduler.schedule(
     #args=[arg1, arg2],             # Arguments passed into function when executed
     #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
     interval=1*60*60,              # Call every 1 hour
-    repeat=10                      # Repeat forever
+    repeat=None                    # Repeat forever
 )
 
 
@@ -33,5 +33,16 @@ default_scheduler.schedule(
     #args=[arg1, arg2],             # Arguments passed into function when executed
     #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
     interval=4*60*60,              # Call every 4 hours
-    repeat=10                      # Repeat forever
+    repeat=None                      # Repeat forever
+)
+
+# Public Event Scheduler
+# Run every 5 minutes
+default_scheduler.schedule(
+    scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+    func=process_onetime_event,                     # Function to be queued
+    #args=[arg1, arg2],             # Arguments passed into function when executed
+    #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+    interval=3*60,                  # Call every 3 minutes
+    repeat=None                      # Repeat forever
 )
