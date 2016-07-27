cd90b57d3d15dcb1e3d2a5a4a57da9f2fb86ddeb
DayoAyeni <dedayoa@gmail.com>
2016-07-27 01:31:26 +0100

    URLconf for SystemSettingsUpdate view

--- gomez/urls.py
+++ gomez/urls.py
@@ -3,3 +3,14 @@ Created on Jul 13, 2016
 
 @author: Dayo
 '''
+
+from django.conf.urls import url, include
+
+from .views import *
+
+urlpatterns = [
+    url(r'^settings/', include([
+                url(r'^system/(?P<pk>\d+)/$', SystemSettingsUpdateView.as_view(), name='system-settings'),
+                
+    ])),
+]
