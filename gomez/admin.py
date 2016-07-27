a87ea96f4e54faaaae502fd232e0af9863105a08
DayoAyeni <dedayoa@gmail.com>
2016-07-27 01:26:39 +0100

    admin for gomez

--- gomez/admin.py
+++ gomez/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import KITBilling, KITServicePlan, KITSystem
+
+admin.site.register(KITBilling)
+admin.site.register(KITServicePlan)
+admin.site.register(KITSystem)
