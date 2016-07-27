3a5bb0f70f1a3c2c7fb544166cfa5032c43df92f
DayoAyeni <dedayoa@gmail.com>
2016-07-27 01:33:11 +0100

    Implement SystemSettingsUpdateView

--- gomez/views.py
+++ gomez/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import UpdateView, CreateView
 
-# Create your views here.
+from .models import KITSystem
+from .forms import SystemSettingsForm
 
 
 def smslive247_callback():
     pass
+    
+    
+class SystemSettingsUpdateView(UpdateView):
+    
+    model = KITSystem
+    form_class = SystemSettingsForm
+    template_name = 'gomez/system_settings.html'
+    
+    def get_context_data(self, **kwargs):
+        params = super(SystemSettingsUpdateView, self).get_context_data(**kwargs)
+        params["syssetid"] = self.object.pk
+        return params
