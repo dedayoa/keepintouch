a6f561f9b8ad3bb0fe716f0b0306fee80eb5a4f4
DayoAyeni <dedayoa@gmail.com>
2016-07-28 11:35:51 +0100

    IssueFeedback model for Admin

--- gomez/admin.py
+++ gomez/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import KITBilling, KITServicePlan, KITSystem
+from .models import KITBilling, KITServicePlan, KITSystem, IssueFeedback
 
 admin.site.register(KITBilling)
 admin.site.register(KITServicePlan)
 admin.site.register(KITSystem)
+admin.site.register(IssueFeedback)
