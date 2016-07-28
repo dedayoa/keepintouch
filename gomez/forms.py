11f8f40eb83375c2ad6158a1abd28f4e6ae6274c
DayoAyeni <dedayoa@gmail.com>
2016-07-28 11:37:46 +0100

    Form for issue feedback

--- gomez/forms.py
+++ gomez/forms.py
@@ -11,7 +11,7 @@ from crispy_forms.layout import HTML
 from crispy_forms_foundation.layout import ButtonGroup, ButtonHolder, Div,\
                                             Submit, Reset, Button, Layout, Fieldset, Row, Column
                                             
-from .models import KITSystem
+from .models import KITSystem, IssueFeedback
 
 class SystemSettingsForm(forms.ModelForm):
     
@@ -27,3 +27,19 @@ class SystemSettingsForm(forms.ModelForm):
     class Meta:
         model = KITSystem
         fields = ['company_wide_contacts']  
+        
+        
+class IssueFeedback(forms.ModelForm):
+    
+    def __init__(self, *args, **kwargs):
+        super(IssueFeedback, self).__init__(*args, **kwargs)
+        
+        self.helper = FormHelper()
+        self.helper.form_action = '.'
+        self.helper.form_tag = True
+        self.helper.add_input(Submit('submit', _('Submit'), css_class="success float-right"))
+        self.helper.add_input(Reset('reset', _('Reset'), css_class="float-right"))
+    
+    class Meta:
+        model = IssueFeedback
+        fields = ['title','detail','screenshot']
