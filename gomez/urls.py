52dee6f05ae3a7eda30055d039f33c6274c30cd0
DayoAyeni <dedayoa@gmail.com>
2016-07-28 14:23:26 +0100

    URLconf for submit issue feedback

--- gomez/urls.py
+++ gomez/urls.py
@@ -7,10 +7,12 @@ Created on Jul 13, 2016
 from django.conf.urls import url, include
 
 from .views import *
+from .ajax import submit_issue_fb
 
 urlpatterns = [
     url(r'^settings/', include([
                 url(r'^system/(?P<pk>\d+)/$', SystemSettingsUpdateView.as_view(), name='system-settings'),
+                url(r'^system/feedback/$', submit_issue_fb, name='system-user-feedback'),
                 
     ])),
 ]
