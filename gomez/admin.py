629846ecc366cced70031a38092ff350b2e2b354
DayoAyeni <dedayoa@gmail.com>
2016-08-03 14:45:38 +0100

    CustomData in admin

--- gomez/admin.py
+++ gomez/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import KITBilling, KITServicePlan, KITSystem, IssueFeedback
+from .models import KITBilling, KITServicePlan, KITSystem, IssueFeedback, CustomData
 
 admin.site.register(KITBilling)
 admin.site.register(KITServicePlan)
 admin.site.register(KITSystem)
 admin.site.register(IssueFeedback)
+admin.site.register(CustomData)
