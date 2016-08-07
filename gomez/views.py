468eaeee038dce08e07da1655ad0c1110f40d2fc
DayoAyeni <dedayoa@gmail.com>
2016-08-07 22:47:37 +0100

    Implemented CustomDataView

--- gomez/views.py
+++ gomez/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import UpdateView, CreateView
 
-from .models import KITSystem
-from .forms import SystemSettingsForm
+import mimetypes
+
+from django.shortcuts import render
+from django.views.generic import UpdateView, CreateView, TemplateView
+from django.conf import settings
+
+from django_tables2 import RequestConfig
+
+from .models import KITSystem, CustomData
+from .forms import SystemSettingsForm, CustomDataIngestForm
+from .tables import CustomDataStoreTable
 
 
 def smslive247_callback():
@@ -19,3 +26,23 @@ class SystemSettingsUpdateView(UpdateView):
         params = super(SystemSettingsUpdateView, self).get_context_data(**kwargs)
         params["syssetid"] = self.object.pk
         return params
+
+
+
+class CustomDataView(TemplateView):
+    
+    template_name = 'gomez/data_mgmt/custom_data.html'
+    params = {}
+    ingest_form = CustomDataIngestForm()
+    
+    def get(self, request):
+        self.params["ingest_form"] = self.ingest_form
+        self.params['file_max_size'] = settings.MAX_UPLOAD_FILE_SIZE
+        self.params["title"] = "Custom Data Management"
+                
+        custdatable = CustomDataStoreTable(CustomData.objects.filter(created_by = request.user.kituser).order_by('-created'))
+        RequestConfig(request, paginate={'per_page': 30}).configure(custdatable)
+        self.params["table"] = custdatable
+        
+        
+        return render(request, self.template_name, self.params)
