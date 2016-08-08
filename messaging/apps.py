59cbc4d0db0cec467bca001eb11eec0da76e900f
DayoAyeni <dedayoa@gmail.com>
2016-08-09 00:52:39 +0100

    moved schedules into apps

--- messaging/apps.py
+++ messaging/apps.py
@@ -1,5 +1,49 @@
 from django.apps import AppConfig
 
+from datetime import datetime
+import django_rq
+from rq_scheduler import Scheduler
+
+from .tasks import process_private_anniversary, process_public_anniversary, process_onetime_event
+
 
 class MessagingConfig(AppConfig):
     name = 'messaging'
+
+
+    def ready(self):
+        default_scheduler = django_rq.get_scheduler('default')
+        
+        # Private Event Scheduler
+        # Run every 1 hour
+        default_scheduler.schedule(
+            scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+            func=process_private_anniversary,                     # Function to be queued
+            #args=[arg1, arg2],             # Arguments passed into function when executed
+            #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+            interval=1*60*60,              # Call every 1 hour
+            repeat=None                    # Repeat forever
+        )
+        
+        
+        # Public Event Scheduler
+        # Run every 4 hour
+        default_scheduler.schedule(
+            scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+            func=process_public_anniversary,                     # Function to be queued
+            #args=[arg1, arg2],             # Arguments passed into function when executed
+            #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+            interval=4*60*60,              # Call every 4 hours
+            repeat=None                      # Repeat forever
+        )
+        
+        # Public Event Scheduler
+        # Run every 5 minutes
+        default_scheduler.schedule(
+            scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+            func=process_onetime_event,                     # Function to be queued
+            #args=[arg1, arg2],             # Arguments passed into function when executed
+            #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+            interval=3*60,                  # Call every 3 minutes
+            repeat=None                      # Repeat forever
+        )
