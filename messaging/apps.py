b5743662f88a150d2eb3c04441fbc245d4ad7bfe
DayoAyeni <dedayoa@gmail.com>
2016-08-09 02:15:17 +0100

    modified apps.

--- messaging/apps.py
+++ messaging/apps.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import django_rq
 from rq_scheduler import Scheduler
 
-from .tasks import process_private_anniversary, process_public_anniversary, process_onetime_event
+#from .tasks import process_private_anniversary, process_public_anniversary, process_onetime_event
 
 
 class MessagingConfig(AppConfig):
@@ -18,7 +18,7 @@ class MessagingConfig(AppConfig):
         # Run every 1 hour
         default_scheduler.schedule(
             scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-            func=process_private_anniversary,                     # Function to be queued
+            func='messaging.tasks.process_private_anniversary',                     # Function to be queued
             #args=[arg1, arg2],             # Arguments passed into function when executed
             #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
             interval=1*60*60,              # Call every 1 hour
@@ -30,7 +30,7 @@ class MessagingConfig(AppConfig):
         # Run every 4 hour
         default_scheduler.schedule(
             scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-            func=process_public_anniversary,                     # Function to be queued
+            func='messaging.tasks.process_public_anniversary',                     # Function to be queued
             #args=[arg1, arg2],             # Arguments passed into function when executed
             #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
             interval=4*60*60,              # Call every 4 hours
@@ -41,9 +41,9 @@ class MessagingConfig(AppConfig):
         # Run every 5 minutes
         default_scheduler.schedule(
             scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-            func=process_onetime_event,                     # Function to be queued
+            func='messaging.tasks.process_onetime_event',                     # Function to be queued
             #args=[arg1, arg2],             # Arguments passed into function when executed
             #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
-            interval=3*60,                  # Call every 3 minutes
+            interval=60,                  # Call every 3 minutes
             repeat=None                      # Repeat forever
         )
