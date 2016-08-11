e02e045f04e124549d462aaa42cf6b7227a704e6
DayoAyeni <dedayoa@gmail.com>
2016-08-11 02:07:20 +0100

    let's try again...

--- runner.py
+++ runner.py
@@ -5,8 +5,9 @@ Created on Aug 10, 2016
 '''
 
 import subprocess
+from sys import stdout
 
 if __name__ == "__main__":
         
-    proc1 = subprocess.run('python manage.py rqworker default sms email')
-    proc2 = subprocess.run('python manage.py rqscheduler')
+    proc1 = subprocess.run('python manage.py rqworker default sms email', shell=True, stdout = stdout)
+    proc2 = subprocess.run('python manage.py rqscheduler', shell=True, stdout = stdout)
