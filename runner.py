efda783639f4cca474b9f4286027a6ddb27b98c4
DayoAyeni <dedayoa@gmail.com>
2016-08-11 01:47:30 +0100

    barebone

--- runner.py
+++ runner.py
@@ -5,27 +5,8 @@ Created on Aug 10, 2016
 '''
 
 import subprocess
-import time
-import psutil
-
-
-def kill(proc_pid):
-    process = psutil.Process(proc_pid)
-    for proc in process.children(recursive=True):
-        proc.kill()
-    process.kill()
-
 
 if __name__ == "__main__":
         
-    proc1 = subprocess.run('python manage.py rqworker default sms email', shell=True, stdout=subprocess.STDOUT)
-    proc2 = subprocess.run('python manage.py rqscheduler', shell=True, stdout=subprocess.STDOUT)
-    
-    proc_list = [proc1, proc2]
-    
-    try:
-        while True:
-            time.sleep(10)
-    except KeyboardInterrupt:
-        for proc in proc_list:
-            kill(proc)
+    proc1 = subprocess.run('python manage.py rqworker default sms email')
+    proc2 = subprocess.run('python manage.py rqscheduler')
