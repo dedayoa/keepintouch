c86a178685272e832f8459608e265599dc75e361
DayoAyeni <dedayoa@gmail.com>
2016-08-11 02:24:01 +0100

    still on the marra

--- runner.py
+++ runner.py
@@ -5,9 +5,27 @@ Created on Aug 10, 2016
 '''
 
 import subprocess
-from sys import stdout
+import time
+import psutil
+
+
+def kill(proc_pid):
+    process = psutil.Process(proc_pid)
+    for proc in process.children(recursive=True):
+        proc.kill()
+    process.kill()
+
 
 if __name__ == "__main__":
         
-    proc1 = subprocess.run('python manage.py rqworker default sms email', shell=True, stdout = stdout)
-    proc2 = subprocess.run('python manage.py rqscheduler', shell=True, stdout = stdout)
+    proc1 = subprocess.Popen('python manage.py rqworker default sms email', shell=True)
+    proc2 = subprocess.Popen('python manage.py rqscheduler', shell=True)
+    
+    proc_list = [proc1, proc2]
+    
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        for proc in proc_list:
+            kill(proc)
