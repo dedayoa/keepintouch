e7b3e3af414346542d098b1122f901d45b0fd218
DayoAyeni <dedayoa@gmail.com>
2016-08-15 15:46:31 +0100

    revived scheduler.py

--- messaging/scheduler.py
+++ messaging/scheduler.py
@@ -9,40 +9,60 @@ import django_rq
 from rq_scheduler import Scheduler
 
 from datetime import datetime
-from .tasks import process_private_anniversary, process_public_anniversary, process_onetime_event
 
 default_scheduler = django_rq.get_scheduler('default')
 
-# Private Event Scheduler
-# Run every 1 hour
-default_scheduler.schedule(
-    scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-    func=process_private_anniversary,                     # Function to be queued
-    #args=[arg1, arg2],             # Arguments passed into function when executed
-    #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
-    interval=1*60*60,              # Call every 1 hour
-    repeat=None                    # Repeat forever
-)
 
-
-# Public Event Scheduler
-# Run every 4 hour
-default_scheduler.schedule(
-    scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-    func=process_public_anniversary,                     # Function to be queued
-    #args=[arg1, arg2],             # Arguments passed into function when executed
-    #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
-    interval=4*60*60,              # Call every 4 hours
-    repeat=None                      # Repeat forever
-)
-
-# Public Event Scheduler
-# Run every 5 minutes
-default_scheduler.schedule(
-    scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-    func=process_onetime_event,                     # Function to be queued
-    #args=[arg1, arg2],             # Arguments passed into function when executed
-    #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
-    interval=3*60,                  # Call every 3 minutes
-    repeat=None                      # Repeat forever
-)
+def run_schedules():
+    # Delete any existing jobs in the scheduler when the app starts up
+    for job in default_scheduler.get_jobs():
+        job.delete()
+    
+    
+    # Private Event Scheduler
+    # Run every 1 hour
+    job_36k = default_scheduler.schedule(
+        scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+        func='messaging.tasks.process_private_anniversary',                     # Function to be queued
+        #args=[arg1, arg2],             # Arguments passed into function when executed
+        #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+        interval=3600,              # Call every 1 hour
+        repeat=None                    # Repeat forever
+    )
+    
+    
+    # Public Event Scheduler
+    # Run every 4 hours
+    job_436k = default_scheduler.schedule(
+        scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+        func='messaging.tasks.process_public_anniversary',                     # Function to be queued
+        #args=[arg1, arg2],             # Arguments passed into function when executed
+        #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+        interval=14400,              # Call every 4 hours
+        repeat=None                      # Repeat forever
+    )
+    
+    # Public Event Scheduler
+    # Run every 5 minutes
+    job_60 = default_scheduler.schedule(
+        scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+        func='messaging.tasks.process_onetime_event',                     # Function to be queued
+        #args=[arg1, arg2],             # Arguments passed into function when executed
+        #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+        interval=60,                  # Call every 3 minutes
+        repeat=None                      # Repeat forever
+    )
+    
+    # Reminder Messages Scheduler
+    # Run once a day
+    job_864k = default_scheduler.schedule(
+        scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+        func='messaging.tasks.process_reminder_event',                     # Function to be queued
+        #args=[arg1, arg2],             # Arguments passed into function when executed
+        #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+        interval=86400,                  # Call every 5 minutes
+        repeat=None                      # Repeat forever
+    )
+    
+    return (job_36k, job_436k, job_60, job_864k)
+    
