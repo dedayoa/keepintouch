7c3e15528651d0ed1d6ee060fa256bf18088bb24
DayoAyeni <dedayoa@gmail.com>
2016-08-18 18:37:24 +0100

    In billing, show only administrators

--- gomez/admin.py
+++ gomez/admin.py
@@ -4,7 +4,15 @@ from django.contrib import admin
 
 from .models import KITBilling, KITServicePlan, KITSystem, IssueFeedback
 
-admin.site.register(KITBilling)
+class KITBillingAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        qs = super(KITBillingAdmin, self).get_queryset(request)
+        
+        return qs.filter(kit_admin__is_admin=True)
+
+
+admin.site.register(KITBilling, KITBillingAdmin)
 admin.site.register(KITServicePlan)
 admin.site.register(KITSystem)
 admin.site.register(IssueFeedback)
