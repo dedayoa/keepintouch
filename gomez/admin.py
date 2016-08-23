e07298e9eb696f14583cffaefcd7aef3a65fb4cb
DayoAyeni <dedayoa@gmail.com>
2016-08-23 21:47:06 +0100

    Extend PrefixAdmin of country_dialcode
    Implemented ImportExport
    Removed IssueFeedback

--- gomez/admin.py
+++ gomez/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import KITBilling, KITServicePlan, KITSystem, IssueFeedback
+from import_export.admin import ImportExportMixin
+from country_dialcode.models import Prefix
+from country_dialcode.admin import PrefixAdmin
+from .models import KITBilling, KITServicePlan, KITSystem, EmailReport, SMSReport, \
+                    SMSRateTable
+
+class MyPrefixAdmin(ImportExportMixin, PrefixAdmin):
+    pass
 
 class KITBillingAdmin(admin.ModelAdmin):
 
@@ -11,8 +18,27 @@ class KITBillingAdmin(admin.ModelAdmin):
         
         return qs.filter(kit_admin__is_admin=True)
 
+    
+    
+class SMSRateTableAdmin(ImportExportMixin, admin.ModelAdmin):
+    
+    list_display = ("dialcode","sms_units")
+
+
+class EmailReportAdmin(admin.ModelAdmin):
+    
+    list_display = ('__str__','status','created')
+    
+class SMSReportAdmin(admin.ModelAdmin):
+    
+    list_display = ('__str__','status','created')
 
 admin.site.register(KITBilling, KITBillingAdmin)
 admin.site.register(KITServicePlan)
 admin.site.register(KITSystem)
-admin.site.register(IssueFeedback)
+admin.site.register(SMSRateTable, SMSRateTableAdmin)
+admin.site.register(EmailReport, EmailReportAdmin)
+admin.site.register(SMSReport, SMSReportAdmin)
+admin.site.unregister(Prefix)
+admin.site.register(Prefix, MyPrefixAdmin)
+
