456c752a07c463014ce8d2c7f5156e93e9b89c73
DayoAyeni <dedayoa@gmail.com>
2016-08-23 21:59:57 +0100

    removed submit_issue_fb

--- gomez/urls.py
+++ gomez/urls.py
@@ -7,12 +7,11 @@ Created on Jul 13, 2016
 from django.conf.urls import url, include
 
 from .views import *
-from .ajax import submit_issue_fb
 
 urlpatterns = [
     url(r'^settings/', include([
                 url(r'^system/(?P<pk>\d+)/$', SystemSettingsUpdateView.as_view(), name='system-settings'),
-                url(r'^system/feedback/$', submit_issue_fb, name='system-user-feedback'),
+                
                 
     ])),
 ]
