5af1aeb601f7a97c3136e2e50bced252c87da4bd
DayoAyeni <dedayoa@gmail.com>
2016-08-23 22:01:51 +0100

    Cache SMTP connection, fleshen SMSHelper class, cleaned up SMSLiveHelper class

--- messaging/helper.py
+++ messaging/helper.py
@@ -3,7 +3,7 @@ Created on Jul 9, 2016
 
 @author: Dayo
 '''
-import sys
+import sys, os
 import requests
 from dateutil.relativedelta import relativedelta
 
@@ -12,107 +12,93 @@ from django.utils import timezone
 from django.core import mail
 from django.core.mail import send_mail, EmailMessage
 from django.core.mail.backends.smtp import EmailBackend
-
+from django.contrib.auth.models import User
 from django.core.validators import validate_email
-from .models import SMSReport, EmailReport
-from core.models import KITUser
+
+#from gomez.models import KITBilling
+from core.models import KITUser, SMTPSetting
+from core.exceptions import *
+
+from cacheops import cached_as
+from django.apps import apps
+
+from gomez.helper import temp_log_to_db, KITRateEngineA
+from messaging.sms_counter import SMSCounter
+from gomez.models import KITBilling
 
 
 
-
-
-def temp_log_to_db(m_type, **kwargs):
-    
-    if m_type == 'email':
-        EmailReport.objects.create(
-            to_email = kwargs['email_msg'][2],
-            from_email = kwargs.get('sender_mail',''),
-            status = 0,
-            owner = kwargs.get('owner',''),
-            email_message = {
-                'title' : kwargs['email_msg'][0],
-                'body' : kwargs['email_msg'][1]
-                           },
-            email_gateway = {
-                'email_id' :'',
-                'gateway_id' : '',
-                'gateway_error_preamble' : kwargs.get('gw_err_preamble','')
-                
-                           }
-        )
-    
-    elif m_type == 'sms':
-        SMSReport.objects.create(
-            to_phone = kwargs['sms_msg'][2],
-            status = 0,
-            owner = kwargs.get('owner',''),
-            sms_message = {
-                'sender_id' : kwargs['sms_msg'][0],
-                'body' : kwargs['sms_msg'][1],
-                'message_type' : '0'
-                           },
-            sms_gateway = {
-                'message_id' : kwargs.get('message_id', 0),
-                'gateway_id' : '',
-                'gateway_error_code' : kwargs.get('message_err_code','')
-                           },
-        )
-    
-    
-    
+#@cached_as(KITBilling, KITUser, timeout=4*3600)        
+def is_company_wide(kuser):
+    if kuser.is_admin and not kuser.kitbilling.is_full_admin:
+        #is an admin, but not billed as a full admin e.g user on free plan
+        return True
+    elif kuser.is_admin and kuser.kitbilling.is_full_admin:
+        raise FailedSendingMessageError("Admin User Cannot Send Messages")
+    elif not kuser.is_admin:
+        return kuser.parent.kitsystem.company_wide_contacts
 
 
 class SMTPHelper():
     
     def __init__(self, smtp_server_profile):
         
-        self.smtp_server = getattr(smtp_server_profile,'smtp_server',None)
+        self.id = getattr(smtp_server_profile, 'id', None)
+        
+        self.smtp_server = getattr(smtp_server_profile,'smtp_server', None)
         self.from_sender = getattr(smtp_server_profile,'from_user','')
-        self.smtp_port = getattr(smtp_server_profile,'smtp_port',25)
+        self.smtp_port = getattr(smtp_server_profile,'smtp_port', 25)
         self.connection_security = getattr(smtp_server_profile,'connection_security','')
         self.smtp_user = getattr(smtp_server_profile,'smtp_user','')
         self.smtp_password = getattr(smtp_server_profile,'smtp_password','')
     
     # will need to cache this later
-    def _get_connection(self):
+    def get_connection(self):
         
-        if self.connection_security == 'SSLTLS':
-            connection = mail.get_connection(
-                    host = self.smtp_server,
-                    port = self.smtp_port,
-                    username = self.smtp_user,
-                    password = self.smtp_password,
-                    use_ssl = True,
-                    timeout=30
-                    )
-            return connection
+        @cached_as(SMTPSetting.objects.filter(pk=self.id), timeout=3600)
+        def _get_connection():
+            
+            if self.connection_security == 'SSLTLS':
+                connection = mail.get_connection(
+                        host = self.smtp_server,
+                        port = self.smtp_port,
+                        username = self.smtp_user,
+                        password = self.smtp_password,
+                        use_ssl = True,
+                        timeout=30
+                        )
+                return connection
+            
+            elif self.connection_security == 'STARTTLS':
+                connection = mail.get_connection(
+                        host = self.smtp_server,
+                        port = self.smtp_port,
+                        username = self.smtp_user,
+                        password = self.smtp_password,
+                        use_tls = True,
+                        timeout=30
+                        )
+                return connection
+            
+            else:
+                connection = mail.get_connection(
+                        host = self.smtp_server,
+                        port = self.smtp_port,
+                        username = self.smtp_user,
+                        password = self.smtp_password,
+                        timeout=30
+                        )
+                return connection
         
-        elif self.connection_security == 'STARTTLS':
-            connection = mail.get_connection(
-                    host = self.smtp_server,
-                    port = self.smtp_port,
-                    username = self.smtp_user,
-                    password = self.smtp_password,
-                    use_tls = True,
-                    timeout=30
-                    )
-            return connection
-        
-        else:
-            connection = mail.get_connection(
-                    host = self.smtp_server,
-                    port = self.smtp_port,
-                    username = self.smtp_user,
-                    password = self.smtp_password,
-                    timeout=30
-                    )
-            return connection
+        return _get_connection()
+    
+    
             
     def test_smtp_server(self, **kwargs):
         
         try:
             
-            conn = self._get_connection()                 
+            conn = self.get_connection()                 
             
             with conn as smtp_connection:
                 mail.EmailMessage(
@@ -135,7 +121,7 @@ class SMTPHelper():
     def send_email(self, email_message, **kwargs):
         
         try:
-            conn = self._get_connection() 
+            conn = self.get_connection() 
             
             with conn as smtp_connection:    
                 msg = EmailMessage(
@@ -169,8 +155,123 @@ class SMTPHelper():
 
 class SMSHelper():
     
-    def __init__(self):
-        return SMSLive247Helper()
+    def __init__(self, message, kuser, msg_type, **kwargs):
+        
+        self.sender = message[0]
+        self.destination = message[2]
+        self.sms_message = message[1]
+        self.kuser = kuser
+        self.msg_type = msg_type
+        
+        self.error_code = ""
+        self.success_message_id = ""
+        
+    def _check_sms_can_be_sent(self):
+        # load us
+        #get_user_balance        
+        fsb = self.kuser.kitubalance.free_sms_balance
+        sb = self.kuser.kitubalance.sms_balance
+        
+        #get parent balance
+        kuser_parent = self.kuser.get_parent()
+        p_fsb = kuser_parent.kitubalance.free_sms_balance
+        p_sb = kuser_parent.kitubalance.sms_balance
+        
+        #get cost per sms unit for destination
+        ppsms = KITRateEngineA().get_sms_cost_to_number(self.destination)
+        
+        #sms pages     
+        smsct = SMSCounter().get_messages_count_only(self.sms_message)
+        
+        if is_company_wide(self.kuser):
+            # public and private messages are sent using the admin (main) account; so they don't fail
+            # reminders and one-shot messages are sent using the users account            
+            if self.msg_type == 'reminder_msg' or self.msg_type == 'one_time_msg':
+                if fsb >= (ppsms * smsct):
+                    return ['fsb', fsb-(ppsms * smsct)]
+                elif sb >= (ppsms * smsct):
+                    return ['sb', sb-(ppsms * smsct)]
+                else:
+                    raise NotEnoughBalanceError("Not enough units to send SMS")
+            elif self.msg_type == 'public_anniv_msg' or self.msg_type == 'private_anniv_msg':
+                if p_fsb >= (ppsms * smsct):
+                    return ['p_fsb', p_fsb-(ppsms * smsct)]
+                elif p_sb >= (ppsms * smsct):
+                    return ['p_sb', p_sb-(ppsms * smsct)]
+                else:
+                    raise NotEnoughBalanceError("Admin does not have enough units to send SMS")
+        else:
+            # all messages are sent using the user's account balance
+            if fsb >= (ppsms * smsct):
+                return ['fsb', fsb-(ppsms * smsct)]
+            elif sb >= (ppsms * smsct):
+                return ['sb', sb-(ppsms * smsct)]
+            else:
+                raise NotEnoughBalanceError("Not enough units to send SMS")
+        
+
+        
+        
+    def _update_user_sms_balance(self, balance_acct_debited, amount):
+        # save balance
+        
+        if is_company_wide(self.kuser) and self.msg_type == 'public_anniv_msg' or self.msg_type == 'private_anniv_msg':
+            if balance_acct_debited == 'p_fsb':
+                self.kuser.parent.kitubalance.update(free_sms_balance=amount)
+            elif balance_acct_debited == 'p_sb':
+                self.kuser.parent.kitubalance.update(sms_balance=amount)
+        else:
+            if balance_acct_debited == 'fsb':
+                self.kuser.kitubalance.update(free_sms_balance=amount)
+            elif balance_acct_debited == 'sb':
+                self.kuser.kitubalance.update(sms_balance=amount)
+    
+    
+    def _sms_success_logging_and_all(self, gw_id):        
+        # do all logging        
+        temp_log_to_db(
+            'sms',
+            sms_msg = [self.sender,self.sms_message,self.destination],
+            message_id = self.success_message_id,
+            owner = self.kuser,
+            gateway_id = gw_id
+        )
+    
+    def _sms_failure_logging_and_all(self):        
+        # do all logging        
+        temp_log_to_db(
+            'sms',
+            sms_msg = [self.sender,self.sms_message,self.destination],
+            message_err_code = self.error_code,
+            owner = self.kuser
+        )
+        
+        
+    def send_sms(self):
+        
+        try:
+            result = self._check_sms_can_be_sent()
+            try:
+                # SMSLive247
+                gw_reply = SMSLive247Helper().send_sms(self.sender, self.sms_message, self.destination)
+                if gw_reply.split(':')[0] is not 'OK':
+                    # raise ALERT!!! log this for admin
+                    pass
+                else:
+                    self.success_message_id = (gw_reply.split(':')[1]).strip()
+                    self._sms_success_logging_and_all(gw_id="SMSLive247")
+            except:
+                # exception while sending
+                return(sys.exc_info())
+            else:
+                # sms successfully sent...no exception. Deduct balance
+                self._update_user_sms_balance(result[0], result[1])
+            
+        except NotEnoughBalanceError as e:
+            # saved message to failed table for user
+            print(e.message)
+        
+        
 
 class SMSLive247Helper():
     
@@ -185,37 +286,18 @@ class SMSLive247Helper():
     def sms_login(self):
         pass
     
-    def send_sms(self, message_payload, **kwargs):
-        try:
-            payload = {'cmd': 'sendmsg',
-                       'sessionid': self.sessid,
-                       'message' : message_payload[1],
-                       'sender' : message_payload[0],
-                       'sendto' : message_payload[2][1:], #remove leading +
-                       'msgtype' : 0
-                       }
-            r = requests.post("http://www.smslive247.com/http/index.aspx", data=payload)
-            response_text = r.text
-            if response_text.split(':')[0] is not 'OK':
-                temp_log_to_db(
-                    'sms',
-                    sms_msg = [message_payload[0],message_payload[1],message_payload[2]],
-                    message_err_code = (response_text.split(':')[1]).strip(),
-                    owner = kwargs['owner']
-                )
-            else:
-                messageid = (response_text.split(':')[1]).strip()
-                #OK: 54142800
-                #log message
-                temp_log_to_db(
-                    'sms',
-                    sms_msg = [message_payload[0],message_payload[1],message_payload[2]],
-                    message_id = messageid,
-                    owner = kwargs['owner']
-                )
-            return r.text
-        except:
-            return(sys.exc_info())
+    def send_sms(self, message_payload):
+        payload = {'cmd': 'sendmsg',
+                   'sessionid': self.sessid,
+                   'message' : message_payload[1],
+                   'sender' : message_payload[0],
+                   'sendto' : message_payload[2][1:], #remove leading +
+                   'msgtype' : 0
+                   }
+        r = requests.post("http://www.smslive247.com/http/index.aspx", data=payload)
+            #OK: 54142800
+            #log message
+        return r.text
     
     def get_sms_msg_status(self, messageid):
         
@@ -225,10 +307,56 @@ class SMSLive247Helper():
                        'messageid' : messageid  
                        }
             r = requests.get("http://www.smslive247.com/http/index.aspx", data=payload)
-            return r
+            return r.text
         except:
             pass
+
+
+
     
+
+class OKToSend(object):
+    # used in place of the initial ok_to_send to cater for Free Users
+    
+    def __init__(self, kuser):
+        self.user = kuser.user
+        self.owner = kuser
+
+        
+    def _is_active(self):
+        if self.owner.is_admin and self.owner.kitbilling.is_full_admin:
+            raise FailedSendingMessageError("Admin User Cannot Send Messages")
+        elif self.owner.is_admin and not self.owner.kitbilling.is_full_admin:
+            if self.user.is_active:
+                return True
+            else:
+                raise IsNotActiveError("User is not active")
+        elif not self.owner.is_admin:
+            if self.user.is_active and self.owner.parent.user.is_active:
+                return True            
+            elif not self.user.is_active:
+                raise IsNotActiveError("User is not active")
+            elif not self.user.parent.is_active:
+                raise IsNotActiveError("Parent is not active")
+            
+    def _has_valid_subscription(self):
+        if self.owner.is_admin and self.owner.kitbilling.is_full_admin:
+            raise FailedSendingMessageError("Admin User Cannot Send Messages")
+        elif self.owner.is_admin and not self.owner.kitbilling.is_full_admin:
+            if self.owner.kitbilling.next_due_date >= timezone.now().date():
+                return True
+            else:
+                raise NoActiveSubscriptionError("Subscription has expired")
+        elif not self.owner.is_admin:
+            if self.owner.parent.kitbilling.next_due_date >= timezone.now().date():
+                return True
+            else:
+                raise NoActiveSubscriptionError("Admin Subscription has expired")
+    
+    
+    #@cached_as(User, KITUser, timeout=3600)   
+    def check(self):
+        return self._is_active() and self._has_valid_subscription()
 
 
 def ok_to_send(owner):
@@ -285,3 +413,5 @@ def get_next_delivery_time(repeat_frequency, delivery_time):
         return delivery_time+relativedelta(months=3)
     elif repeat_frequency == "annually":
         return delivery_time+relativedelta(years=1) 
+
+
