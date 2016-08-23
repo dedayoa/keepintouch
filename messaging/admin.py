c1d7af9b946a41d7faa4eb9002364e060434e36b
DayoAyeni <dedayoa@gmail.com>
2016-08-23 21:47:42 +0100

    Moved EmailReport & SMSReport out, moved IssueFeedback in

--- messaging/admin.py
+++ messaging/admin.py
@@ -1,24 +1,16 @@
 from django.contrib import admin
 
 from .models import StandardMessaging, AdvancedMessaging, ProcessedMessages,\
-                    QueuedMessages, EmailReport, SMSReport, ReminderMessaging, Reminder,\
+                    QueuedMessages, IssueFeedback, ReminderMessaging, Reminder,\
                     RunningMessage
 
-class EmailReportAdmin(admin.ModelAdmin):
-    
-    list_display = ('__str__','status','created')
-    
-class SMSReportAdmin(admin.ModelAdmin):
-    
-    list_display = ('__str__','status','created')
 
 
 admin.site.register(StandardMessaging)
 admin.site.register(AdvancedMessaging)
 admin.site.register(ProcessedMessages)
 admin.site.register(QueuedMessages)
-admin.site.register(EmailReport, EmailReportAdmin)
-admin.site.register(SMSReport, SMSReportAdmin)
 admin.site.register(ReminderMessaging)
 admin.site.register(Reminder)
 admin.site.register(RunningMessage)
+admin.site.register(IssueFeedback)
