37aff78140e4dae39afc5c0b393f574c42b2cdde
DayoAyeni <dedayoa@gmail.com>
2016-08-23 21:48:53 +0100

    removed submit_issue_fb

--- gomez/ajax.py
+++ gomez/ajax.py
@@ -17,24 +17,8 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile, File
 from django.utils.text import slugify
 
-from .forms import IssueFeedbackForm
 #from munch import munchify
 
 
-
-@ajax
-@login_required
-def submit_issue_fb(request):
-    
-    if request.method == "POST":
-        
-        form = IssueFeedbackForm(request.POST, request.FILES)
-        if not form.is_valid():
-            return {'errors':form.errors.as_json(escape_html=True)}
-        else:
-            obj = form.save(commit=False)
-            obj.submitter = request.user.kituser
-            obj.save()
-            return {'result':'Feedback Received.<br />Thank You'}
         
         
