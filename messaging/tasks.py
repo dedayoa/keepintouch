64582915257037f98dfc3e8ee35222624baed126
DayoAyeni <dedayoa@gmail.com>
2016-08-24 11:22:51 +0100

    update tasks to use OKToSend instead of ok_to_send

--- messaging/tasks.py
+++ messaging/tasks.py
@@ -5,17 +5,21 @@ Created on Jun 15, 2016
 '''
 
 from django.template import Context, Template
+from django.apps import apps
 import logging
 import django_rq
 
 from django.utils import timezone
 from django.conf import settings
-from core.models import Event, PublicEvent, Contact, SMTPSetting,\
-    KITUser
-from .helper import SMTPHelper, SMSHelper, ok_to_send, SMSLive247Helper, get_next_delivery_time
+
+from .helper import SMTPHelper, SMSHelper, get_next_delivery_time, OKToSend
+from core.exceptions import *
+from core.models import Contact, PublicEvent, KITUser, SMTPSetting, Event
 from dateutil.relativedelta import relativedelta
 
-from .models import QueuedMessages, ProcessedMessages,RunningMessage
+from .models import QueuedMessages, ProcessedMessages,RunningMessage, IssueFeedback
+
+#SMTPSetting = apps.get_model('core','SMTPSetting')
 
 def _compose(template, convars):
     
@@ -40,64 +44,82 @@ def _send_mass_email(email_message, smtp_profile):
     es.send_mass_email(email_message)
 
 @django_rq.job('sms')
-def _send_sms(sms_message, **kwargs):
-    es = SMSLive247Helper()
-    print(es.send_sms(sms_message, **kwargs))
+def _send_sms(sms_message, kuser, msg_type, **kwargs):
+    es = SMSHelper(sms_message, kuser, msg_type, **kwargs)
+    print(es.send_sms())
 
-@django_rq.job('sms')   
-def _send_mass_sms(sms_message, **kwargs):
-    # params sms_message: contains all the message payload
-    es = SMSHelper()
-    es.send_mass_sms(sms_message)
 
 def process_private_anniversary():
     #get anniversaries that occur today and which have not been handled
+    
     due_private_events = Event.objects.select_related('contact','message').filter(date__day = timezone.now().day,
                                                                       date__month = timezone.now().month,
                                                                       last_run__year__lte = timezone.now().year)
     # For each due private anniversary, prepare email and sms
     
     for peven in due_private_events:
-        if ok_to_send(peven.contact.kit_user):
-            if peven.message.send_email and peven.contact.email and peven.message.email_template:
-                etempl = _compose(peven.message.email_template, peven.contact) #compose title
-                rttempl = _compose(peven.message.title, peven.contact) #compose message
-                e_job = _send_email.delay([rttempl,etempl,peven.contact.email],\
-                                          peven.message.smtp_setting.values(),\
-                                          owner = peven.contact.kit_user
-                                          )
-                
-            if peven.message.send_sms and peven.contact.phone and peven.message.sms_template:
-                sms_msg = _compose(peven.message.sms_template, peven.contact)
-                sender = _compose(peven.message.sms_sender, peven.contact)
-                s_job = _send_sms.delay([sender,sms_msg,peven.contact.phone.as_e164],\
-                                        owner = peven.contact.kit_user
-                                        )
-
-            peven.update(last_run=timezone.now().date()) 
+        try:
+            ok_to_send = OKToSend(peven.contact.kit_user)
+            if ok_to_send.check():
+                if peven.message.send_email and peven.contact.email and peven.message.email_template:
+                    etempl = _compose(peven.message.email_template, peven.contact) #compose title
+                    rttempl = _compose(peven.message.title, peven.contact) #compose message
+                    e_job = _send_email.delay([rttempl,etempl,peven.contact.email],\
+                                              peven.message.smtp_setting.values(),\
+                                              owner = peven.contact.kit_user
+                                              )
+                    
+                if peven.message.send_sms and peven.contact.phone and peven.message.sms_template:
+                    sms_msg = _compose(peven.message.sms_template, peven.contact)
+                    sender = _compose(peven.message.sms_sender, peven.contact)
+                    s_job = _send_sms.delay([sender,sms_msg,peven.contact.phone.as_e164],\
+                                            peven.contact.kit_user,
+                                            'private_anniv_msg'
+                                            )
+    
+                peven.update(last_run=timezone.now().date())
+        # move messages to a paused queue pending when error is resolved
+        # also, send user a message about the issue
+        except IsNotActiveError as e:
+            print(e.message)
+        except NoActiveSubscriptionError as e:
+            print(e.message)
+        except FailedSendingMessageError as e:
+            print(e.message)
+             
     
     
 def process_public_anniversary():
     #get anniversaries that occur today
+    
     due_public_events = PublicEvent.objects.select_related('message').filter(date = timezone.now().date())
     # For each due private anniversary,
     for publicevent in due_public_events:
         for recipient_d in publicevent.get_recipients():
-            if ok_to_send(publicevent.kit_user):
-                if publicevent.message.send_email and recipient_d.email and publicevent.message.email_template:
-                    e_msg = _compose(publicevent.message.email_template, recipient_d)
-                    e_title = _compose(publicevent.message.title, recipient_d)
-                    _send_email.delay([e_title,e_msg,recipient_d.email],\
-                                      publicevent.message.smtp_setting.values(),\
-                                      owner = publicevent.kit_user
-                                      )
-                    
-                if publicevent.message.send_sms and recipient_d.phone and publicevent.message.sms_template:
-                    s_msg = _compose(publicevent.message.sms_template, recipient_d)
-                    s_sender = _compose(publicevent.message.sms_sender, recipient_d)
-                    _send_sms.delay([s_sender,s_msg,recipient_d.phone.as_e164],\
-                                    owner = publicevent.kit_user
-                                    )
+            try:
+                ok_to_send = OKToSend(publicevent.kit_user)
+                if ok_to_send.check():
+                    if publicevent.message.send_email and recipient_d.email and publicevent.message.email_template:
+                        e_msg = _compose(publicevent.message.email_template, recipient_d)
+                        e_title = _compose(publicevent.message.title, recipient_d)
+                        _send_email.delay([e_title,e_msg,recipient_d.email],\
+                                          publicevent.message.smtp_setting.values(),\
+                                          owner = publicevent.kit_user
+                                          )
+                        
+                    if publicevent.message.send_sms and recipient_d.phone and publicevent.message.sms_template:
+                        s_msg = _compose(publicevent.message.sms_template, recipient_d)
+                        s_sender = _compose(publicevent.message.sms_sender, recipient_d)
+                        _send_sms.delay([s_sender,s_msg,recipient_d.phone.as_e164],\
+                                        publicevent.kit_user,
+                                        'public_anniv_msg'
+                                        )
+            except IsNotActiveError as e:
+                print(e.message)
+            except NoActiveSubscriptionError as e:
+                print(e.message)
+            except FailedSendingMessageError as e:
+                print(e.message)
             
         publicevent.update(date = timezone.now().date()+relativedelta(years=1))
     
@@ -106,25 +128,37 @@ def process_onetime_event():
     due_queued_messages = QueuedMessages.objects.filter(delivery_time__lte = timezone.now())
     
     for queued_message in due_queued_messages:
+        
+        
         recipients_qs = Contact.objects.filter(pk__in = queued_message.message["recipients"])
         
         for recipient_d in recipients_qs:
-            if ok_to_send(queued_message.created_by):
-                #email
-                if queued_message.message["send_email"] and recipient_d.email and queued_message.message["email_template"]:
-                    smtp_setting_qsv = SMTPSetting.objects.get(pk = queued_message.message["smtp_setting_id"])
-                    e_msg = _compose(queued_message.message["email_template"], recipient_d)
-                    e_title = _compose(queued_message.message["title"], recipient_d)
-                    _send_email.delay([e_title, e_msg, recipient_d.email],\
-                                      smtp_setting_qsv, owner = queued_message.created_by
-                                      )
-                #sms   
-                if queued_message.message["send_sms"] and recipient_d.phone and queued_message.message["sms_template"]:
-                    s_msg = _compose(queued_message.message["sms_template"], recipient_d)
-                    s_sender = _compose(queued_message.message["sms_sender_id"], recipient_d)
-                    _send_sms.delay([s_sender, s_msg, recipient_d.phone.as_e164],\
-                                      owner = queued_message.created_by
-                                      )
+            try:
+                ok_to_send = OKToSend(queued_message.created_by)
+                if ok_to_send.check():
+                    #email
+                    if queued_message.message["send_email"] and recipient_d.email and queued_message.message["email_template"]:
+                        smtp_setting_qsv = SMTPSetting.objects.get(pk = queued_message.message["smtp_setting_id"])
+                        e_msg = _compose(queued_message.message["email_template"], recipient_d)
+                        e_title = _compose(queued_message.message["title"], recipient_d)
+                        _send_email.delay([e_title, e_msg, recipient_d.email],\
+                                          smtp_setting_qsv, owner = queued_message.created_by
+                                          )
+                    #sms   
+                    if queued_message.message["send_sms"] and recipient_d.phone and queued_message.message["sms_template"]:
+                        s_msg = _compose(queued_message.message["sms_template"], recipient_d)
+                        s_sender = _compose(queued_message.message["sms_sender_id"], recipient_d)
+                        _send_sms.delay([s_sender, s_msg, recipient_d.phone.as_e164],\
+                                         queued_message.created_by,
+                                         'one_time_msg'
+                                          )
+                        
+            except IsNotActiveError as e:
+                print(e.message)
+            except NoActiveSubscriptionError as e:
+                print(e.message)
+            except FailedSendingMessageError as e:
+                print(e.message)
         
         # create entry in processed message
         ProcessedMessages.objects.create(
@@ -157,22 +191,33 @@ def process_reminder_event():
             created_by = KITUser.objects.get(pk=message_due[4])
             recipient_d = Contact.objects.get(pk=message_due[1])
             
-            if ok_to_send(created_by):
-                #email
-                if message["send_email"] and recipient_d.email and message["email_template"]:
-                    smtp_setting_qsv = SMTPSetting.objects.get(pk = message["smtp_setting_id"])
-                    e_msg = _compose(message["email_template"], recipient_d)
-                    e_title = _compose(message["title"], recipient_d)
-                    _send_email.delay([e_title, e_msg, recipient_d.email],\
-                                      smtp_setting_qsv, owner = created_by
-                                      )
-                #sms
-                if message["send_sms"] and recipient_d.phone and message["sms_template"]:
-                    s_msg = _compose(message["sms_template"], recipient_d)
-                    s_sender = _compose(message["title"], recipient_d)
-                    _send_sms.delay([s_sender, s_msg, recipient_d.phone.as_e164],\
-                                      owner = created_by
-                                      )
+            try:
+                ok_to_send = OKToSend(created_by)
+                if ok_to_send.check():
+                    #email
+                    if message["send_email"] and recipient_d.email and message["email_template"]:
+                        smtp_setting_qsv = SMTPSetting.objects.get(pk = message["smtp_setting_id"])
+                        e_msg = _compose(message["email_template"], recipient_d)
+                        e_title = _compose(message["title"], recipient_d)
+                        _send_email.delay([e_title, e_msg, recipient_d.email],\
+                                          smtp_setting_qsv, owner = created_by
+                                          )
+                    #sms
+                    if message["send_sms"] and recipient_d.phone and message["sms_template"]:
+                        s_msg = _compose(message["sms_template"], recipient_d)
+                        s_sender = _compose(message["title"], recipient_d)
+                        _send_sms.delay([s_sender, s_msg, recipient_d.phone.as_e164],\
+                                         created_by,
+                                         'reminder_msg'
+                                          )
+                        
+            except IsNotActiveError as e:
+                print(e.message)
+            except NoActiveSubscriptionError as e:
+                print(e.message)
+            except FailedSendingMessageError as e:
+                print(e.message)
+                
             # create entry in processed message
             ProcessedMessages.objects.create(
                 message_type = 'REMINDER',
@@ -180,9 +225,7 @@ def process_reminder_event():
                 created_by = created_by
             )  
                 
-        
 
-        
         
 def process_system_notification(**kwargs):
     
@@ -242,4 +285,6 @@ def process_system_notification(**kwargs):
                       smtp_settings,
                       owner = KITUser.objects.get(pk=settings.DEVS_KITUSER_ID) #current admin
                       )
-    
+
+
+
