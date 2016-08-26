768e69675581d5a1147226a1f4dd941185c8b834
DayoAyeni <dedayoa@gmail.com>
2016-08-26 09:36:05 +0100

    admin for the SMSDelivery...

--- reportng/admin.py
+++ reportng/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import SMSDeliveryReport, SMSDeliveryReportHistory
+
+admin.site.register(SMSDeliveryReport)
+admin.site.register(SMSDeliveryReportHistory)
