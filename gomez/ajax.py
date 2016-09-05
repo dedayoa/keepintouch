d1339602f8889d0b3940090e0f2d705daf6f9b91
DayoAyeni <dedayoa@gmail.com>
2016-09-05 19:07:30 +0100

    removed unused imports

--- gomez/ajax.py
+++ gomez/ajax.py
@@ -4,18 +4,6 @@ Created on Jul 28, 2016
 @author: Dayo
 '''
 
-import tablib
-import uuid
-import os
-import base64
-from cryptography.fernet import Fernet
-
-from django.contrib.auth.decorators import login_required
-from django_ajax.decorators import ajax
-from django.conf import settings
-from django.core.files.storage import default_storage
-from django.core.files.base import ContentFile, File
-from django.utils.text import slugify
 
 #from munch import munchify
 
