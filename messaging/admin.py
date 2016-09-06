7c2a5b96d7ff4421fbc0dc251ac6ee793b01ee3b
DayoAyeni <dedayoa@gmail.com>
2016-09-06 01:49:42 +0100

    admin gets the failed message models

--- messaging/admin.py
+++ messaging/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 from .models import StandardMessaging, AdvancedMessaging, ProcessedMessages,\
                     QueuedMessages, IssueFeedback, ReminderMessaging, Reminder,\
-                    RunningMessage
+                    RunningMessage, FailedEmailMessage, FailedSMSMessage, FailedKITMessage
 
 
 
@@ -13,4 +13,7 @@ admin.site.register(QueuedMessages)
 admin.site.register(ReminderMessaging)
 admin.site.register(Reminder)
 admin.site.register(RunningMessage)
+admin.site.register(FailedEmailMessage)
+admin.site.register(FailedSMSMessage)
+admin.site.register(FailedKITMessage)
 admin.site.register(IssueFeedback)
