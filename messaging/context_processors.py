5e57d96b44cb512cc9a3c4b511d40c51046d4185
DayoAyeni <dedayoa@gmail.com>
2016-09-08 16:23:46 +0100

    context processor to display badge for total_failed_messages_count

--- messaging/context_processors.py
+++ messaging/context_processors.py
@@ -5,9 +5,28 @@ Created on Jul 28, 2016
 '''
 
 from .forms import IssueFeedbackForm
+from cacheops import cached_as
+from .models import FailedEmailMessage, FailedKITMessage, FailedSMSMessage
 
 
 def issue_form_processor(request):
     form = IssueFeedbackForm()
     
     return {'issueform': form}
+
+
+@cached_as(FailedEmailMessage, FailedSMSMessage, FailedKITMessage, timeout=120)
+def total_failed_messages_count(request):
+    
+    if request.user.kituser.is_admin:
+        t_email = FailedEmailMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+        t_sms = FailedSMSMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+        t_kitm = FailedKITMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+    else:
+        t_email = FailedEmailMessage.objects.filter(owned_by = request.user.kituser).count()
+        t_sms = FailedSMSMessage.objects.filter(owned_by = request.user.kituser).count()
+        t_kitm = FailedKITMessage.objects.filter(owned_by = request.user.kituser).count()
+    
+    return {'total_failed_msg_count' : t_email+t_kitm+t_sms}
+    
+    
