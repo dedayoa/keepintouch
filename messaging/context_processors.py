3ba4d0caf40af46e8c7130343b3bf12a886e9786
DayoAyeni <dedayoa@gmail.com>
2016-09-08 17:58:49 +0100

    reworked caching so it invalidates per user, not the entire db

--- messaging/context_processors.py
+++ messaging/context_processors.py
@@ -15,21 +15,29 @@ def issue_form_processor(request):
     return {'issueform': form}
 
 
-@cached_as(FailedEmailMessage, FailedSMSMessage, FailedKITMessage, timeout=120)
+
 def total_failed_messages_count(request):
     
-    if hasattr(request.user, 'kituser'):
-        if request.user.kituser.is_admin:
-            t_email = FailedEmailMessage.objects.filter(owned_by__parent = request.user.kituser).count()
-            t_sms = FailedSMSMessage.objects.filter(owned_by__parent = request.user.kituser).count()
-            t_kitm = FailedKITMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+    @cached_as(FailedEmailMessage.objects.filter(owned_by = request.user.kituser), 
+               FailedSMSMessage.objects.filter(owned_by = request.user.kituser), 
+               FailedKITMessage.objects.filter(owned_by = request.user.kituser),
+               timeout=120) 
+    def _total_failed_msgs():
+    
+        if hasattr(request.user, 'kituser'):
+            if request.user.kituser.is_admin:
+                t_email = FailedEmailMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+                t_sms = FailedSMSMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+                t_kitm = FailedKITMessage.objects.filter(owned_by__parent = request.user.kituser).count()
+            else:
+                t_email = FailedEmailMessage.objects.filter(owned_by = request.user.kituser).count()
+                t_sms = FailedSMSMessage.objects.filter(owned_by = request.user.kituser).count()
+                t_kitm = FailedKITMessage.objects.filter(owned_by = request.user.kituser).count()
+            
+            return {'total_failed_msg_count' : t_email+t_kitm+t_sms}
         else:
-            t_email = FailedEmailMessage.objects.filter(owned_by = request.user.kituser).count()
-            t_sms = FailedSMSMessage.objects.filter(owned_by = request.user.kituser).count()
-            t_kitm = FailedKITMessage.objects.filter(owned_by = request.user.kituser).count()
+            return {}
         
-        return {'total_failed_msg_count' : t_email+t_kitm+t_sms}
-    else:
-        return {}
+    return _total_failed_msgs()
     
     
