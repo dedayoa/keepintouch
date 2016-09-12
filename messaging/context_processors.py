26637cb5879752b5723869dc1a0c8b153c744ba3
DayoAyeni <dedayoa@gmail.com>
2016-09-12 22:16:42 +0100

    fixed a small issue with total_failed_messages_count hasattr

--- messaging/context_processors.py
+++ messaging/context_processors.py
@@ -18,13 +18,14 @@ def issue_form_processor(request):
 
 def total_failed_messages_count(request):
     
-    @cached_as(FailedEmailMessage.objects.filter(owned_by = request.user.kituser), 
-               FailedSMSMessage.objects.filter(owned_by = request.user.kituser), 
-               FailedKITMessage.objects.filter(owned_by = request.user.kituser),
-               timeout=120) 
-    def _total_failed_msgs():
+    if hasattr(request.user, 'kituser'):
     
-        if hasattr(request.user, 'kituser'):
+        @cached_as(FailedEmailMessage.objects.filter(owned_by = request.user.kituser), 
+                   FailedSMSMessage.objects.filter(owned_by = request.user.kituser), 
+                   FailedKITMessage.objects.filter(owned_by = request.user.kituser),
+                   timeout=120) 
+        def _total_failed_msgs():
+        
             if request.user.kituser.is_admin:
                 t_email = FailedEmailMessage.objects.filter(owned_by__parent = request.user.kituser).count()
                 t_sms = FailedSMSMessage.objects.filter(owned_by__parent = request.user.kituser).count()
@@ -35,9 +36,10 @@ def total_failed_messages_count(request):
                 t_kitm = FailedKITMessage.objects.filter(owned_by = request.user.kituser).count()
             
             return {'total_failed_msg_count' : t_email+t_kitm+t_sms}
-        else:
-            return {}
         
-    return _total_failed_msgs()
+        return _total_failed_msgs()
+    
+    else:
+        return {}
     
     
