6cc742fc83db17fab95c9785a967488f7dc810a2
DayoAyeni <dedayoa@gmail.com>
2016-09-16 15:22:50 +0100

    implemented ready to run_schedules

--- reportng/apps.py
+++ reportng/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
 
+from reportng.scheduler import run_schedules
+
 
 class ReportngConfig(AppConfig):
     name = 'reportng'
+
+
+    def ready(self):
+        run_schedules()
