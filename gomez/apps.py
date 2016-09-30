958db818fc895606caf0beedeab411a9a896e690
DayoAyeni <dedayoa@gmail.com>
2016-09-30 20:57:39 +0100

    activate running of rq_scheduler schedules

--- gomez/apps.py
+++ gomez/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from gomez.scheduler import run_schedules
 
 class GomezConfig(AppConfig):
     name = 'gomez'
+
+
+    def ready(self):
+        run_schedules()
