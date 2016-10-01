6aa59bb531ea146951beece88b03204028e232a1
DayoAyeni <dedayoa@gmail.com>
2016-10-01 22:57:41 +0100

    Revert "wish I had a linux PC though, I'd be able to test this."
    This reverts commit fe2154d9df36eeb358aa3a1a1c084cc1a746e9ea.

--- gomez/scheduler.py
+++ gomez/scheduler.py
@@ -20,7 +20,7 @@ def run_schedules():
     #    job.delete()
     # expire verification codes that have been created over 24hours
     
-    arg1 = default_scheduler.schedule(
+    default_scheduler.schedule(
         scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
         func='gomez.tasks.expire_validation_code',
         interval=3600, 
@@ -29,7 +29,7 @@ def run_schedules():
     
     # User SMS Balance crediting
     # Run every month
-    arg2 = default_scheduler.schedule(
+    default_scheduler.schedule(
         #scheduled_time=arrow.utcnow().ceil('day').replace(seconds=+1).datetime, # Time for first execution, in UTC timezone
         scheduled_time=datetime.utcnow().replace(day=datetime.utcnow().day+1, hour=0, minute=0, second=0),
         func='gomez.tasks.process_monthly_sms_crediting',                     # Function to be queued
@@ -37,5 +37,3 @@ def run_schedules():
         interval=86400,              # Call once a day
         repeat=None                  # Repeat forever
     )
-    
-    return (arg1, arg2)
