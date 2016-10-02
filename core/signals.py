9cf28eced9600a57d2388787dd9057bcdee16ce6
DayoAyeni <dedayoa@gmail.com>
2016-10-02 10:46:04 +0100

    moved free_trial_user_signup_callback signal from messaging to core

--- core/signals.py
+++ core/signals.py
@@ -4,13 +4,20 @@ Created on Aug 29, 2016
 @author: Dayo
 '''
 import sys
+
 from django.db import transaction
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-
-from .models import KITUser, KITUBalance, CoUserGroup
 from django.apps import apps
 from django.utils import timezone
+from django.contrib.auth.models import Group
+from django.conf import settings
+
+from .models import KITUser, KITUBalance, CoUserGroup
+
+from sitegate.signals import sig_user_signup_success
+from gomez.models import KITServicePlan
+import arrow
 
 
 @receiver(post_save, sender=KITUser)
@@ -54,4 +61,38 @@ def create_and_set_default_user_group(sender, instance, *args, **kwargs):
                 instance.user_group = coug
         except:
             print("Error:", sys.exc_info())
+            
+
+
+@receiver(sig_user_signup_success)                
+def free_trial_user_signup_callback(signup_result, flow, request, **kwargs):
+    if request.path == '/register/free-trial/':
+        #disconnect the post_save signal for KITUser
+        post_save.disconnect(create_kituser_assoc_tables, sender=KITUser)
+        
+        # create new KITUser
+        KITUser.objects.create(user=signup_result, is_admin=True)
+        
+        # connect again
+        post_save.connect(create_kituser_assoc_tables, sender=KITUser)
+        
+
+        free_trial_service_plan = KITServicePlan.objects.get(id=settings.FREE_TRIAL_SERVICE_PLAN_ID)
+        
+        kitbilling = apps.get_model('gomez', 'KITBilling')
+        kitbilling.objects.create(
+                kit_admin=signup_result.kituser,
+                service_plan = free_trial_service_plan,
+                next_due_date = arrow.utcnow().replace(days=settings.FREE_TRIAL_VALIDITY_PERIOD).datetime.date(),
+                registered_on = timezone.now().date(),
+                account_status = 'AC'
+                )
+        
+        kitsystem = apps.get_model('gomez', 'KITSystem')
+        kitsystem.objects.create(kit_admin=signup_result.kituser)
+        
+        KITUBalance.objects.create(kit_user=signup_result.kituser, sms_balance=settings.FREE_TRIAL_FREE_SMS_UNITS)
+        #set free user permissions
+        group = Group.objects.get(id=settings.FREE_TRIAL_GROUP_PERMS_ID)
+        signup_result.groups.add(group)
   
--- messaging/signals.py
+++ messaging/signals.py
@@ -4,9 +4,7 @@ Created on Aug 24, 2016
 @author: Dayo
 '''
 
-import arrow
 from django.db import transaction
-from django.contrib.auth.models import User, Group
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.apps import apps
@@ -15,11 +13,8 @@ from django.utils import timezone
 from .models import IssueFeedback
 from .tasks import process_system_notification, process_verification_messages
 
-from core.models import KITActivationCode, KITUser, KITUBalance
-from sitegate.signals import sig_user_signup_success
-from django.conf import settings
-from gomez.models import KITServicePlan
-from core.signals import create_kituser_assoc_tables
+from core.models import KITActivationCode
+
         
 
 @receiver(post_save, sender=IssueFeedback)
@@ -73,39 +68,8 @@ def process_sending_verification_details(sender, instance, **kwargs):
                     email_verification_code = email_verification_code,
                                         )
         transaction.on_commit(on_commit)
+   
 
-
-@receiver(sig_user_signup_success)       
-def free_trial_user_signup_callback(signup_result, flow, request, **kwargs):
-    if request.path == '/register/free-trial/':
-        #disconnect the post_save signal for KITUser
-        post_save.disconnect(create_kituser_assoc_tables, sender=KITUser)
-        
-        # create new KITUser
-        KITUser.objects.create(user=signup_result, is_admin=True)
-        
-        # connect again
-        post_save.connect(create_kituser_assoc_tables, sender=KITUser)
-        
-
-        free_trial_service_plan = KITServicePlan.objects.get(id=settings.FREE_TRIAL_SERVICE_PLAN_ID)
-        
-        kitbilling = apps.get_model('gomez', 'KITBilling')
-        kitbilling.objects.create(
-                kit_admin=signup_result.kituser,
-                service_plan = free_trial_service_plan,
-                next_due_date = arrow.utcnow().replace(days=settings.FREE_TRIAL_VALIDITY_PERIOD).datetime.date(),
-                registered_on = timezone.now().date(),
-                account_status = 'AC'
-                )
-        
-        kitsystem = apps.get_model('gomez', 'KITSystem')
-        kitsystem.objects.create(kit_admin=signup_result.kituser)
-        
-        KITUBalance.objects.create(kit_user=signup_result.kituser, sms_balance=settings.FREE_TRIAL_FREE_SMS_UNITS)
-        #set free user permissions
-        group = Group.objects.get(id=settings.FREE_TRIAL_GROUP_PERMS_ID)
-        signup_result.groups.add(group)
 '''
     
 @receiver(post_save, sender=User)
