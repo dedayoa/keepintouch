8cf613478f08aadf5b8bbf233d0aef3402553e67
DayoAyeni <dedayoa@gmail.com>
2016-10-05 11:29:07 +0100

    implement EmailReport table

--- reportng/tables.py
+++ reportng/tables.py
@@ -5,16 +5,32 @@ Created on Aug 3, 2016
 '''
 
 import os, json
+import re
 
 import django_tables2 as tables
 from django_tables2.utils import A
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html, html_safe
 
-from .models import SMSDeliveryReport, SMSDeliveryReportHistory
+from .models import SMSDeliveryReport, SMSDeliveryReportHistory, EmailDeliveryReport
 from django.core.urlresolvers import reverse, reverse_lazy
 
+import html2text
 
+
+
+
+def text_2_wordlist(text, max_number_of_words):
+    text_list = re.sub("[^\w]", " ",  text).split()
+    return " ".join(text_list[0:max_number_of_words])+"..." if len(text_list) > max_number_of_words else text
+
+
+def html_to_text(htmlmsg):    
+    jtext = html2text.HTML2Text()
+    jtext.ignore_links = True
+    jtext.ignore_tables = True
+    jtext.ignore_anchors = True
+    return jtext.handle(htmlmsg)
 
 
 class SMSReportTable(tables.Table):
@@ -51,5 +67,25 @@ class SMSReportTable(tables.Table):
     class Meta:
         model = SMSDeliveryReport
         fields = ('sms_message','sms_sender','to_phone','msg_info','created')
+        empty_text = 'There are no Reports to display.'
+        attrs = {'style': 'width: 100%'}
+
+
+        
+class EmailReportTable(tables.Table):
+    
+    email_message = tables.Column(verbose_name='Email')
+    from_email = tables.EmailColumn(verbose_name="From")
+    to_email = tables.EmailColumn(verbose_name="To")
+    msg_status = tables.Column(verbose_name='Status')
+    created = tables.Column(verbose_name="Sent")
+    
+    
+    def render_email_message(self, record):
+        return format_html('<a href="#">{}</a>',text_2_wordlist(html_to_text(record.email_message.get('title')), 5))
+    
+    class Meta:
+        model = EmailDeliveryReport
+        fields = ('email_message','from_email','to_email','msg_status','created')
         empty_text = 'There are no Reports to display.'
         attrs = {'style': 'width: 100%'}
