08cb5c3fb61c54051bf2a1fd9cdfbcbf3dc9aaaa
DayoAyeni <dedayoa@gmail.com>
2016-10-23 02:57:32 +0100

    url confs for order page

--- gomez/urls.py
+++ gomez/urls.py
@@ -10,8 +10,18 @@ from .views import *
 
 urlpatterns = [
     url(r'^settings/', include([
-                url(r'^my/system/$', SystemSettingsUpdateView.as_view(), name='system-settings'),
+        url(r'^my/system/$', SystemSettingsUpdateView.as_view(), name='system-settings'),
+        url(r'^account/expired/$', SubscriptionExpired.as_view(), name='account-expired'),
                 
                 
+    ])),
+               
+    url(r'^order/', include([
+            url(r'^start/$', OrderStart.as_view(), name='order-start'),
+            url(r'^(?P<order_id>[A-Z0-9]{10})/$', OrderCart.as_view(), name='order-cart'),
+            url(r'^(?P<order_id>[A-Z0-9]{10})/update/$', UpdateCart.as_view(), name='order-update'),
+            url(r'^(?P<order_id>[A-Z0-9]{10})/item/(?P<id>\d+)/delete/$', DeleteCartItem.as_view(), name='order-delete-item'),
+            url(r'^checkout/$', OrderCheckout.as_view(), name='order-checkout'),
+            #url(r'^paynow/$', get_qpc_stats, name='order-checkout'),
     ])),
 ]
