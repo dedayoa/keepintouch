597991005b35627c38b6700104c7151b8af292fe
DayoAyeni <dedayoa@gmail.com>
2016-11-08 00:37:58 +0100

    urlconf for cdr webhook

--- reportng/urls.py
+++ reportng/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     url(r'^sms/$', SMSReport.as_view(), name='sms-reports'),
     url(r'^email/$', EmailReport.as_view(), name='email-reports'),
     url(r'^sms/delivery/infobip/$', infobip_sms_delivery_report_callback, name='infobip-delivery-url'),
+    url(r'^webhook/cdr/$', fs_call_detail_report_callback, name='fs-cdr-url'),
 ]
