51c8009fa50dd488e9515838f89a022772b93243
DayoAyeni <dedayoa@gmail.com>
2016-11-08 12:51:08 +0100

    added call_uuid model field

--- reportng/models.py
+++ reportng/models.py
@@ -150,6 +150,7 @@ class CallDetailReportTransaction(models.Model):
     #date_generated = models.DateTimeField()
     date_received = models.DateTimeField(auto_now_add=True)
     
+    call_uuid = models.CharField(max_length=100)
     body = JSONField()
     request_meta = JSONField()
     
