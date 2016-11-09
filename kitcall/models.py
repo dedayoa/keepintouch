473148e5ca7da2b957ce80dcfbd172f9399027ae
DayoAyeni <dedayoa@gmail.com>
2016-11-09 22:46:20 +0100

    implemented CallHistory and CallStatus models

--- kitcall/models.py
+++ kitcall/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class CallStatus(models.Model):
+    
+    job_uuid = models.UUIDField(primary_key=True)
+    status = models.TextField(blank=True)
+    
+    caller = models.ForeignKey('core.KITUser', on_delete=models.SET_NULL, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+class CallHistory(models.Model):
+    # formerly FailedCall Model
+    
+    status = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, blank=True)
+    
+    caller = models.ForeignKey('core.KITUser', on_delete=models.SET_NULL, null=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
