a1a9e80fd8dde0d3e3b77dc93babafb64c2c34b4
DayoAyeni <dedayoa@gmail.com>
2016-11-10 18:43:01 +0100

    implemented call_number function

--- kitcall/ajax.py
+++ kitcall/ajax.py
@@ -6,9 +6,16 @@ Created on Nov 8, 2016
 
 from django.contrib.auth.decorators import login_required
 from django_ajax.decorators import ajax
-
+from .helper import CallHelper, return_all_level_err
 
 @ajax
 @login_required
 def call_number(request, pn):
-    pass
+    
+    if request.method == "GET":
+        k = CallHelper(pn, request.user.kituser)
+        resp = k.make_my_call()
+        if resp[0] == 0:            
+            return {'result':resp[1]}
+        else:
+            return {'errors':return_all_level_err(resp[1])}
