5098d27ece740e277c3b159e7678d52f8fc02677
DayoAyeni <dedayoa@gmail.com>
2016-11-29 23:23:30 +0100

    handle exception which occurs when same uuid is sent from FS server

--- reportng/webhooks.py
+++ reportng/webhooks.py
@@ -3,6 +3,9 @@ Created on Sep 14, 2016
 
 @author: Dayo
 '''
+
+import logging
+
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.http.response import HttpResponse
@@ -11,6 +14,10 @@ from django.conf import settings
 
 import json, copy
 from .models import SMSDeliveryReportTransaction, CallDetailReportTransaction
+from django.db.utils import IntegrityError
+
+
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
@@ -42,6 +49,14 @@ def fs_call_detail_report_callback(request):
         if not isinstance(v, str):
             del meta[k]
     
-    CallDetailReportTransaction.objects.create(call_uuid = calluid, body = jdata, request_meta = meta)
+    # crazy sip spammers are flooding my FS server. I can't use a firewall (because app is on heroku -wtout public ip)
+    # at the same time I want things to be as fast as possible, so no key/id comparisions - even from memory
+    # so if the sender tries to duplicate the uuid and cause those massive errors I'm seeing, just ignore, but tell him
+    # all went well. That's a bandwidth hog though so I need to figure out how to block them at source
+    try:
+        CallDetailReportTransaction.objects.create(call_uuid = calluid, body = jdata, request_meta = meta)
+    except IntegrityError:
+        logger.error('** Duplicate UUID: {} was sent'.format(calluid))
+        return HttpResponse(status=200)
     
     return HttpResponse(status=200)
