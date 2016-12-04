8600323b94cb34675bf1a4c135d1d2385019438f
DayoAyeni <dedayoa@gmail.com>
2016-12-04 22:51:26 +0100

    created the email delivery report scheduler

--- reportng/scheduler.py
+++ reportng/scheduler.py
@@ -25,7 +25,16 @@ def run_schedules():
 
     default_scheduler.schedule(
         scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
-        func='reportng.tasks.process_deliveryreport_transaction',                   # Function to be queued
+        func='reportng.tasks.process_sms_deliveryreport_transaction',                   # Function to be queued
+        #args=[arg1, arg2],             # Arguments passed into function when executed
+        #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
+        interval=120,              # Call every 2 minutes
+        repeat=None                    # Repeat forever
+    )
+    
+    default_scheduler.schedule(
+        scheduled_time=datetime.utcnow(), # Time for first execution, in UTC timezone
+        func='reportng.tasks.process_email_deliveryreport_transaction',                   # Function to be queued
         #args=[arg1, arg2],             # Arguments passed into function when executed
         #kwargs={'foo': 'bar'},         # Keyword arguments passed into function when executed
         interval=120,              # Call every 2 minutes
