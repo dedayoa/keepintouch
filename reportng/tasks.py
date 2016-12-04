bd093e8fa0fcbdb5338160f7a8520ebc54af2f2b
DayoAyeni <dedayoa@gmail.com>
2016-12-04 22:52:07 +0100

    implemented the process_email_deliveryreport_transaction

--- reportng/tasks.py
+++ reportng/tasks.py
@@ -5,10 +5,12 @@ Created on Sep 16, 2016
 '''
 
 import logging
+import arrow
 
-from .models import SMSDeliveryReportTransaction, SMSDeliveryReport
+from .models import SMSDeliveryReportTransaction, SMSDeliveryReport, EmailReportTransaction, EmailDeliveryReport,\
+                    EmailReceiverAction
 
-def process_deliveryreport_transaction():
+def process_sms_deliveryreport_transaction():
     
     # get all unprocessed report transactions
     all_unprocessed_rt = SMSDeliveryReportTransaction.objects.filter(status='0')
@@ -27,3 +29,60 @@ def process_deliveryreport_transaction():
         # now update the status of the SMS delivery report transaction.
         u_rt.status = '1'
         u_rt.save()
+        
+        
+def process_email_deliveryreport_transaction():
+    
+    def email_event_fxn(email_dr_id, email_status, sent_at=0):
+        
+        if sent_at != 0:
+            # convert unix timestamp to utc datetune
+            sent_at_pydt = arrow.get(sent_at).datetime
+             
+            EmailDeliveryReport.objects.filter(id=email_dr_id).update(
+                    msg_status = email_status,
+                    sent_at = sent_at_pydt
+                    )
+        else:
+            EmailDeliveryReport.objects.filter(id=email_dr_id).update(
+                    msg_status = email_status
+                    )
+    
+    def recepient_action_fxn(email_dr_id, action_id, action_timestamp, report):
+        
+        # convert unix timestamp to utc datetune
+        action_t = arrow.get(action_timestamp).datetime
+        
+        EmailReceiverAction.objects.filter(email_delivery_report=email_dr_id).update(
+                    action = action_id,
+                    action_time = action_t,
+                    extra_data = report
+                    )
+    
+    # get all unprocessed report transactions
+    all_unprocessed_edr = EmailReportTransaction.objects.filter(status='0')
+    
+    for edr in all_unprocessed_edr:
+        
+        for report in edr.body:
+            if report['event'] == 'delivered':
+                email_event_fxn(report['email_id'],EmailDeliveryReport.E_DELIVERED)
+            if report['event'] == 'processed':
+                email_event_fxn(report['email_id'],EmailDeliveryReport.E_SENT)
+            if report['event'] == 'dropped':
+                email_event_fxn(report['email_id'],EmailDeliveryReport.E_DROPPED)
+            if report['event'] == 'bounced':
+                email_event_fxn(report['email_id'],EmailDeliveryReport.E_BOUNCED)
+            if report['event'] == 'deferred':
+                email_event_fxn(report['email_id'],EmailDeliveryReport.E_DEFERRED)
+            
+            if report['event'] == 'open':
+                recepient_action_fxn(report['email_id'], 1, report['timestamp'],report)
+            if report['event'] == 'click':
+                recepient_action_fxn(report['email_id'], 2, report['timestamp'],report)
+            if report['event'] == 'spamreport':
+                recepient_action_fxn(report['email_id'], 7, report['timestamp'],report)
+            
+        # now update the status of the SMS delivery report transaction.
+        edr.status = '1'
+        edr.save()
